04b7e170529827444d20af9cc29be94bd236627f
ChristinaIvanova <37734848+ChristinaIvanova@users.noreply.github.com>
2019-04-22 23:02:58 +0300

    Update Program.cs

--- Technology Fundamentals/03 Arrays/E01 Train/Program.cs
+++ Technology Fundamentals/03 Arrays/E01 Train/Program.cs
@@ -7,9 +7,11 @@ namespace _0001Е_Train
         static void Main(string[] args)
         {
             int countWagons = int.Parse(Console.ReadLine());
+            
             int[] peopleInWagons = new int[countWagons];
-            int peopleInTrain =0;
+            int peopleInTrain = 0;
             string wagon = null;
+            
             for (int i = 0; i < peopleInWagons.Length; i++)
             {
                 peopleInWagons[i] = int.Parse(Console.ReadLine());
@@ -17,6 +19,7 @@ namespace _0001Е_Train
                 wagon = string.Join(' ',peopleInWagons);
                 
             }
+            
             Console.WriteLine($"{wagon}{Environment.NewLine}{peopleInTrain}");
         }
     }

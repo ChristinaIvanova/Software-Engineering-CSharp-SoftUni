f994a489ad6e948c9ebd71193de44c47dcf6df87
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-04-23 22:52:26 +0300

    Add solutions

---
+++ CSharp-Advansed/01-Stacks and Queues/E11 Key Revolver/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace E11_Key_Revolver
+{
+    class Program
+    {
+        static void Main()
+        {
+            var bulletPrice = int.Parse(Console.ReadLine());
+            var sizeGun = int.Parse(Console.ReadLine());
+            var bulletSequence = Console.ReadLine()
+                .Split()
+                .Select(int.Parse);
+            var lockSequence = Console.ReadLine()
+                .Split()
+                .Select(int.Parse);
+            var intelligence = int.Parse(Console.ReadLine());
+
+            var bullets = new Stack<int>(bulletSequence);
+            var locks = new Queue<int>(lockSequence);
+
+            var counterBullets = 0;
+
+            while (locks.Any())
+            {
+                if (bullets.Any())
+                {
+                    var nextBullet = bullets.Pop();
+                    var nextLock = locks.Peek();
+
+                    if (nextBullet <= nextLock)
+                    {
+                        locks.Dequeue();
+                        Console.WriteLine("Bang!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ping!");
+                    }
+
+                    counterBullets++;
+
+                    if (counterBullets % sizeGun == 0 && bullets.Any())
+                    {
+                        Console.WriteLine("Reloading!");
+                    }
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (locks.Any())
+            {
+                var locksLeft = locks.Count;
+                Console.WriteLine($"Couldn't get through. Locks left: {locksLeft}");
+            }
+            else
+            {
+                var bulletsLeft = bullets.Count;
+                var moneyEarned = intelligence - counterBullets * bulletPrice;
+                Console.WriteLine($"{bulletsLeft} bullets left. Earned ${moneyEarned}");
+            }
+        }
+    }
+}
---
+++ CSharp-Advansed/01-Stacks and Queues/E12 Cups and Bottles/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace E12_Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cupsCapacity = Console.ReadLine()
+                .Split()
+                .Select(int.Parse);
+
+            var bottlesCapacity = Console.ReadLine()
+                .Split()
+                .Select(int.Parse);
+
+            var cups = new Queue<int>(cupsCapacity);
+            var bottles = new Stack<int>(bottlesCapacity);
+
+            var wastedWater = 0;
+            bool isEmpty = false;
+
+            while (!isEmpty)
+            {
+                if (bottles.Count == 0 || cups.Count == 0)
+                {
+                    isEmpty = true;
+                    continue;
+                }
+
+                var cup = cups.Peek();
+                while (cup > 0)
+                {
+
+                    var bottle = bottles.Pop();
+                    cup -= bottle;
+                }
+
+                cups.Dequeue();
+                wastedWater += Math.Abs(cup);
+            }
+
+            if (bottles.Any())
+            {
+                var bottlesLeft = string.Join(" ", bottles);
+                Console.WriteLine($"Bottles: {bottlesLeft}");
+            }
+            else if (cups.Any())
+            {
+                var cupsLeft = string.Join(" ", cups);
+                Console.WriteLine($"Cups: {cupsLeft}");
+            }
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}

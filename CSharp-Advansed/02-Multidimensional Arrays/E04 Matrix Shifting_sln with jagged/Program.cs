3c873f0f1ff5c11657fcd7f68ea43fd11bff7388
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-01 12:51:54 +0300

    Add solutions

---
+++ CSharp-Advansed/02-Multidimensional Arrays/E04 Matrix Shifting_sln with jagged/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace E04_Matrix_Shifting
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dimensions = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = dimensions[0];
+            var cols = dimensions[1];
+            string[][] matrix = new string[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                matrix[row] = currentRow;
+            }
+
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                var tokens = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var command = tokens[0];
+
+                if (command != "swap" || tokens.Length != 5)
+                {
+                    Console.WriteLine("Invalid input!");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                var row1 = int.Parse(tokens[1]);
+                var col1 = int.Parse(tokens[2]);
+                var row2 = int.Parse(tokens[3]);
+                var col2 = int.Parse(tokens[4]);
+
+                if (row1 < 0 || row1 > rows - 1 || col1 < 0 || col1 > cols - 1 ||
+                    row2 < 0 || row2 > rows - 1 || col2 < 0 || col2 > cols - 1)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else
+                {
+                    var firstValue = matrix[row1][col1];
+                    var secondValue = matrix[row2][col2];
+
+                    matrix[row1][col1] = secondValue;
+                    matrix[row2][col2] = firstValue;
+
+                    PrintMatrix(rows, cols, matrix);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void PrintMatrix(int rows, int cols, string[][] matrix)
+        {
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
---
+++ CSharp-Advansed/02-Multidimensional Arrays/E05 Snake Moves/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace E05_Snake_Moves
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dimensions = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = dimensions[0];
+            var cols = dimensions[1];
+
+            char[][] jagged = new char[rows][];
+
+            var snakeStr = Console.ReadLine().ToCharArray();
+
+            Queue<char> snakeQueue = new Queue<char>(snakeStr);
+
+            for (int row = 0; row < rows; row++)
+            {
+                jagged[row] = new char[cols];
+
+                for (int col = 0; col < cols; col++)
+                {
+                    char charToAdd = snakeQueue.Dequeue();
+                    jagged[row][col] = charToAdd;
+                    snakeQueue.Enqueue(charToAdd);
+                }
+            }
+
+            foreach (var item in jagged)
+            {
+                Console.WriteLine(string.Join("", item));
+            }
+        }
+    }
+}

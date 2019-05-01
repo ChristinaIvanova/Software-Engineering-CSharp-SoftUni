92c611979be85514409ba8c1ca9dbe0422d117da
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-01 12:55:08 +0300

    Add solutions

---
+++ CSharp-Advansed/02-Multidimensional Arrays/E02 2x2 Squares in Matrix/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace E02_2x2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dimensions = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = dimensions[0];
+            var cols = dimensions[1];
+            var matrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRow = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+            int counterPairs = SquareOfEqualChars(rows, cols, matrix);
+
+            Console.WriteLine(counterPairs);
+        }
+
+        private static int SquareOfEqualChars(int rows, int cols, char[,] matrix)
+        {
+            var counterPairs = 0;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1]
+                        && matrix[row, col] == matrix[row + 1, col]
+                        && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        counterPairs++;
+                    }
+                }
+            }
+
+            return counterPairs;
+        }
+    }
+}
---
+++ CSharp-Advansed/02-Multidimensional Arrays/E03 Maximum Sum/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace E03_Maximum_Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dimension = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = dimension[0];
+            var cols = dimension[1];
+            var matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            var maxSum = int.MinValue;
+            var maxRowIndex = 0;
+            var maxColIndex = 0;
+
+            for (int row = 0; row < rows - 2; row++)
+            {
+                for (int col = 0; col < cols - 2; col++)
+                {
+                    var currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2]
+                        + matrix[row + 1, col] + matrix[row+1, col + 1] + matrix[row + 1, col + 2]
+                        + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                    if (maxSum < currentSum)
+                    {
+                        maxSum = currentSum;
+                        maxRowIndex = row;
+                        maxColIndex = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+            Console.WriteLine($"{matrix[maxRowIndex, maxColIndex]} {matrix[maxRowIndex, maxColIndex + 1]} {matrix[maxRowIndex, maxColIndex + 2]}");
+            Console.WriteLine($"{matrix[maxRowIndex + 1, maxColIndex]} {matrix[maxRowIndex + 1, maxColIndex + 1]} {matrix[maxRowIndex + 1, maxColIndex + 2]}");
+            Console.WriteLine($"{matrix[maxRowIndex + 2, maxColIndex]} {matrix[maxRowIndex + 2, maxColIndex + 1]} {matrix[maxRowIndex + 2, maxColIndex + 2]}");
+        }
+    }
+}

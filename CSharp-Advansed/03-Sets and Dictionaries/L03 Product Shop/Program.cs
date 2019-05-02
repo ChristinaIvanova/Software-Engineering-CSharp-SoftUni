a3ddf5af039907a1909b14f764bd8140849a909f
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-02 14:13:29 +0300

    Add solutions

---
+++ CSharp-Advansed/03-Sets and Dictionaries/L03 Product Shop/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace L03_Product_Shop
+{
+    class Program
+    {
+        static void Main()
+        {
+            var shops = new SortedDictionary<string, Dictionary<string, double>>();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "Revision")
+                {
+                    break;
+                }
+
+                var productParts = line
+                    .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var shop = productParts[0];
+                var product = productParts[1];
+                var price = double.Parse(productParts[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops[shop] = new Dictionary<string, double>();
+                }
+
+                shops[shop][product] = price;
+            }
+
+            foreach (var kvp in shops)
+            {
+                var shop = kvp.Key;
+                var products = kvp.Value;
+
+                Console.WriteLine($"{shop}->");
+                foreach (var productKvp in products)
+                {
+                    Console.WriteLine($"Product: {productKvp.Key}, Price: {productKvp.Value:f1}");
+                }
+            }
+            var set = new SortedSet<string>();
+        }
+    }
+}

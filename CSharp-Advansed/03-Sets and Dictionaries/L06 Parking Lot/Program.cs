744fedd60cb0e9466d3082efccc166990800fdd7
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-02 14:15:00 +0300

    Add solutions

---
+++ CSharp-Advansed/03-Sets and Dictionaries/L06 Parking Lot/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L06_Parking_Lot
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cars = new HashSet<string>();
+
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                var tokens = input.Split(new[] { ',', ' ' }
+                , StringSplitOptions.RemoveEmptyEntries);
+
+                var command = tokens[0];
+                var car = tokens[1];
+
+                switch (command)
+                {
+                    case "IN":
+                        cars.Add(car);
+                        break;
+                    case "OUT":
+                        cars.Remove(car);
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+
+        }
+    }
+}
---
+++ CSharp-Advansed/03-Sets and Dictionaries/L07 SoftUni Party/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L07_SoftUni_Party
+{
+    class Program
+    {
+        static void Main()
+        {
+            var regularGuests = new HashSet<string>();
+            var vipGuests = new HashSet<string>();
+
+            var input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                if (char.IsDigit(input[0]))
+                {
+                    vipGuests.Add(input);
+                }
+                else
+                {
+                    regularGuests.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input!="END")
+            {
+                vipGuests.Remove(input);
+                regularGuests.Remove(input);
+
+                input = Console.ReadLine();
+            }
+
+            var guestNotGoing = vipGuests.Count + regularGuests.Count;
+
+            Console.WriteLine(guestNotGoing);
+            foreach (var guest in vipGuests)
+            {
+                Console.WriteLine(guest);
+            }
+
+            foreach (var guest in regularGuests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}

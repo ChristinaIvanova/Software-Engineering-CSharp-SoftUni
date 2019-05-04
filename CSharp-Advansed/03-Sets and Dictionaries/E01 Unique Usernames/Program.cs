ec8b5dcba01ecb9865a0eaf7cfa631db3f5ea19a
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-04 22:15:18 +0300

    Add solutions

---
+++ CSharp-Advansed/03-Sets and Dictionaries/E01 Unique Usernames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E01_Unique_Usernames
+{
+    class Program
+    {
+        static void Main()
+        {
+            var usernames = new HashSet<string>();
+
+            var numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var username = Console.ReadLine();
+                usernames.Add(username);
+            }
+
+            foreach (var username in usernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}

156e60897399eadcc4ed3bbfd8b41f32fd73bc12
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-04 22:18:20 +0300

    Add solutions

---
+++ CSharp-Advansed/03-Sets and Dictionaries/E04 Even Times/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace E04_Even_Times
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = new Dictionary<int, int>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(number))
+                {
+                    numbers[number] = 0;
+                }
+
+                numbers[number]++;
+            }
+
+            var repeatedEvenTimes = numbers.FirstOrDefault(x => x.Value % 2 == 0);
+            Console.WriteLine($"{repeatedEvenTimes.Key}");
+        }
+    }
+}

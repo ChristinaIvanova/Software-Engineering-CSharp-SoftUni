cb660a69d97b61c93e64f1d707b1d956507421c6
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-04 22:17:13 +0300

    Add solutions

---
+++ CSharp-Advansed/03-Sets and Dictionaries/E03 Periodic Table/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace E03_Periodic_Table
+{
+    class Program
+    {
+        static void Main()
+        {
+            var compounds = new SortedSet<string>();
+            var numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    var compound = line[j];
+                    compounds.Add(compound);
+                }
+            }
+
+            var compounsInString = string.Join(" ", compounds);
+            Console.WriteLine(compounsInString);
+        }
+    }
+}

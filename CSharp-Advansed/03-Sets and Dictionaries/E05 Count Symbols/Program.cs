70dc4024be0dae21d7efdaee05129e82a5903b9c
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-04 22:19:38 +0300

    Add solutions

---
+++ CSharp-Advansed/03-Sets and Dictionaries/E05 Count Symbols/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E05_Count_Symbols
+{
+    class Program
+    {
+        static void Main()
+        {
+            var symbols = new Dictionary<char, int>();
+
+            var text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var symbol = text[i];
+
+                if (!symbols.ContainsKey(symbol))
+                {
+                    symbols[symbol] = 0;
+                }
+
+                symbols[symbol]++;
+            }
+
+            foreach (var kvp in symbols.OrderBy(x => x.Key))
+            {
+                var symbol = kvp.Key;
+                var repeatedTimes = kvp.Value;
+
+                Console.WriteLine($"{symbol}: {repeatedTimes} time/s");
+            }
+        }
+    }
+}

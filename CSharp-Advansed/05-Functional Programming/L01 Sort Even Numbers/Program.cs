ecdfd26179daa7b2a9dc5201e64c1d8da98b3e4a
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-08 22:28:51 +0300

    Add solutions

---
+++ CSharp-Advansed/05-Functional Programming/L01 Sort Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace L01_Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                 .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .Where(x => x % 2 == 0)
+                 .OrderBy(x => x);
+
+            var result = string.Join(", ", numbers);
+            Console.WriteLine(result);
+        }
+    }
+}

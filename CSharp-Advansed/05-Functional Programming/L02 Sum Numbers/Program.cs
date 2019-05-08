5baa91e56987327ba0054ea63d0b2456b26a2cb9
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-08 22:30:15 +0300

    Add solutions

---
+++ CSharp-Advansed/05-Functional Programming/L02 Sum Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace L02_Sum_Numbers
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Func<string, int> parser = n => int.Parse(n);
+
+            int[] numbers = input.Split(new string[] { ", " },
+                       StringSplitOptions.RemoveEmptyEntries)
+                       .Select(parser).ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+
+
+        }
+
+    }
+}

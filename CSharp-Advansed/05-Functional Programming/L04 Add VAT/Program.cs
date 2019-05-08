21f21262d6419b857a7880227726cb49e143e61f
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-08 22:32:49 +0300

    Add solutions

---
+++ CSharp-Advansed/05-Functional Programming/L04 Add VAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+
+namespace L04_Add_VAT
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine()
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x * 1.2)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x:f2}"));
+        }
+    }
+}

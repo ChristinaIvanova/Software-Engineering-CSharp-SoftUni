ae95e553b336000fa3f08a736d5922499adf1b7e
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-10 22:07:22 +0300

    Add solutions

---
+++ CSharp-Advansed/04-Streams and Directories/E06 Full Directory Traversal/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace E06_Full_Directory_Traversal
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path = Console.ReadLine();
+
+            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+            var extensionFileInfo = new Dictionary<string, List<FileInfo>>();
+
+            foreach (var file in files)
+            {
+                FileInfo info = new FileInfo(file);
+
+                if (!extensionFileInfo.ContainsKey(info.Extension))
+                {
+                    extensionFileInfo[info.Extension] = new List<FileInfo>();
+                }
+
+                extensionFileInfo[info.Extension].Add(info);
+            }
+
+            var pathToDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"/Report.txt";
+
+            using (var writer = new StreamWriter(pathToDesktop))
+            {
+                foreach (var kvp in extensionFileInfo.OrderByDescending(x=>x.Value.Count)
+                    .ThenBy(x=>x.Key))
+                {
+                    var ext = kvp.Key;
+                    var info = kvp.Value;
+
+                    writer.WriteLine(ext);
+                    foreach (var fileInfo in info.OrderByDescending(x=>x.Length))
+                    {
+                        var name = fileInfo.Name;
+                        var size = fileInfo.Length / 1024;
+
+                        writer.WriteLine($"--{name} - {size:f3}kb");
+                    }
+                }
+            }
+
+        }
+    }
+}

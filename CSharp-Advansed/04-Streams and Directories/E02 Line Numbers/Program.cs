91a9cadfe1a39ec097df6ab2ddb85ed066d9ff14
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-10 22:01:59 +0300

    Add solutions

---
+++ CSharp-Advansed/04-Streams and Directories/E02 Line Numbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace E02_Line_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var reader = new StreamReader("../../../text.txt"))
+            {
+                var counter = 1;
+
+                using (var writer=new StreamWriter("../../../output.txt"))
+                {
+                    while (true)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line==null)
+                        {
+                            break;
+                        }
+                        var counterLetters = 0;
+                        var counterPunct = 0;
+
+                        foreach (var character in line)
+                        {
+                            if (char.IsLetter(character))
+                            {
+                                counterLetters++;
+                            }
+                            else if (char.IsPunctuation(character))
+                            {
+                                counterPunct++;
+                            }
+                        }
+
+                        writer.WriteLine($"Line{counter}: {line} ({counterLetters})({counterPunct})");
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}

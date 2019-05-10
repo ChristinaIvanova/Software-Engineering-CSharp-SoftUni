b342e9fa089b515a8f6352aaf03416416cf319c9
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-10 22:09:43 +0300

    Add solutions

---
+++ CSharp-Advansed/04-Streams and Directories/L03 Word Count/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L03_Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

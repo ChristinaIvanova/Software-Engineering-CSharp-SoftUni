821367c128856338c85549c82df88322fe5bec75
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-11 22:51:48 +0300

    Add solutions

---
+++ CSharp-Advansed/05-Functional Programming/E03 Custom Min Function/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E03_Custom_Min_Function
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToList();
+            
+            Func<List<int>, int> findMin = x => x.Min();
+
+            Console.WriteLine(findMin(numbers));
+        }
+    }
+}

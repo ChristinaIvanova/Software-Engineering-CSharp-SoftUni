0e6a3543ecbc918691eb71d2c01658d2d73ed686
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-11 22:56:16 +0300

    Add solutions

---
+++ CSharp-Advansed/05-Functional Programming/E05 Applied Arithmetics sol2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace E05_Applied_Arithmetics_sol2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var command = Console.ReadLine();
+
+            Action<string> arithmetics = operation =>
+              {
+                  switch (operation)
+                  {
+                      case "add":
+                          numbers = numbers.Select(n => n + 1).ToList();
+                          break;
+                      case "multiply":
+                          numbers = numbers.Select(n => n * 2).ToList();
+                          break;
+                      case "subtract":
+                          numbers = numbers.Select(n => n - 1).ToList();
+                          break;
+                      case "print":
+                          Console.WriteLine(string.Join(" ", numbers));
+                          break;
+                  }
+              };
+
+            while (command!="end")
+            {
+                arithmetics(command);
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}

ab5cbba7270e970de19e0c7cd0f16fc76163497b
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-11 22:50:09 +0300

    Add solutions

---
+++ CSharp-Advansed/05-Functional Programming/E01 Action Point/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E01_Action_Point
+{
+    class Program
+    {
+        static void Main()
+        {
+            var names = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Action<List<string>> print = x => Console.WriteLine(string.Join
+                (Environment.NewLine, x));
+
+            print(names);
+        }
+    }
+}

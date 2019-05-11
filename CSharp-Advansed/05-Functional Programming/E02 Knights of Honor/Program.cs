6ef2e3efe7e8b748a444d0082f2559091eb30bd3
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-11 22:50:54 +0300

    Add solutions

---
+++ CSharp-Advansed/05-Functional Programming/E02 Knights of Honor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E02_Knights_of_Honor
+{
+    class Program
+    {
+        static void Main()
+        {
+            var names = Console.ReadLine()
+                 .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                 .ToList();
+
+            Func<string, string> format = x => $"Sir {x}";
+
+            Action<List<string>> print = x => Console.WriteLine(string.Join
+                (Environment.NewLine, x.Select(format)));
+
+            print(names);
+        }
+    }
+}

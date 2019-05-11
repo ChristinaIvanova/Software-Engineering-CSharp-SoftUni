cca18f49113804c7b04008c40e68dac7f10dde43
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-11 22:54:02 +0300

    Add solutions

---
+++ CSharp-Advansed/05-Functional Programming/E04 Find Evens or Odds/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E04_Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main()
+        {
+            var rangeArea = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var start = rangeArea[0];
+            var end = rangeArea[1];
+
+            var typeNumbers = Console.ReadLine();
+
+            var numbers = new List<int>();
+
+            Predicate<int> filter = x => typeNumbers == "odd" ? x % 2 != 0 : x % 2 == 0;
+
+            for (int number = start; number <= end; number++)
+            {
+                numbers.Add(number);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers.Where(x => filter(x))));
+        }
+    }
+}

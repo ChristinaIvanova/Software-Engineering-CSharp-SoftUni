494ba38613a34026a45311f9b9bda3d462e75029
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-13 23:16:32 +0300

    Add solutions

---
+++ CSharp-Advansed/05-Functional Programming/E09 List of Predicates/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E09_List_of_Predicates
+{
+    class Program
+    {
+        static void Main()
+        {
+            var endOfRange = int.Parse(Console.ReadLine());
+            var divisors = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var divisibleNumbers = GetDivisibleNumbers(endOfRange, divisors);
+
+            Console.WriteLine(string.Join(" ", divisibleNumbers));
+        }
+
+        private static List<int> GetDivisibleNumbers(int endOfRange, List<int> divisors)
+        {
+            var divisibleNumbers = new List<int>();
+
+            for (int number = 1; number <= endOfRange; number++)
+            {
+                var isDivisible = true;
+
+                foreach (var divisor in divisors)
+                {
+                    Predicate<int> isNotDivisor = x => number % x != 0;
+
+                    if (isNotDivisor(divisor))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    divisibleNumbers.Add(number);
+                }
+            }
+
+            return divisibleNumbers;
+        }
+    }
+}

9bb781905b856c5c321267bd840a457214ce46a7
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-17 06:57:46 +0300

    Add solution

--- CSharp-Advansed/01-Stacks and Queues/E10 Crossroads/Program.cs
+++ CSharp-Advansed/01-Stacks and Queues/E10 Crossroads/Program.cs
@@ -1,56 +1,59 @@
-﻿namespace P01_StacksAndQueues
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _10
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-
-	class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int greenLightLength = int.Parse(Console.ReadLine());
-            int freeWindow = int.Parse(Console.ReadLine());
+            var green = int.Parse(Console.ReadLine());
+            var freeWindow = int.Parse(Console.ReadLine());
 
-            Queue<string> lane = new Queue<string>();
-            int totalCarsPassed = 0;
+            var trafficQueue = new Queue<string>();
 
-            string input;
-            while((input = Console.ReadLine()) != "END")
+            var input = Console.ReadLine();
+            var totalCarsPassed = 0;
+
+            while (input != "END")
             {
                 if (input != "green")
                 {
-                    lane.Enqueue(input);
-                    continue;
+                    trafficQueue.Enqueue(input);
                 }
-
-                int currentGreenLight = greenLightLength;
-
-                string passingCar = null;
-                while (currentGreenLight > 0 && lane.Any())
+                else
                 {
-                    passingCar = lane.Dequeue();
-                    currentGreenLight -= passingCar.Length;
-                    totalCarsPassed++;
+                    string car = string.Empty;
+                    var currentGreen = green;
+
+                    while (trafficQueue.Any() && currentGreen > 0)
+                    {
+                        car = trafficQueue.Dequeue();
+                        currentGreen -= car.Length;
+                        totalCarsPassed++;
+                    }
+
+                    var leftWindowTime = freeWindow + currentGreen;
+                    if (leftWindowTime < 0)
+                    {
+                        var indexOfHit = car.Length + leftWindowTime;
+                        var symbolOfHit = car[indexOfHit];
+                        Crash(car, symbolOfHit);
+                    }
                 }
 
-                int freeWindowLeft = freeWindow + currentGreenLight;
-                if (freeWindowLeft < 0)
-                {
-                    int symbolsThatDidntPass = Math.Abs(freeWindowLeft);
-                    int indexOfHitSymbol = passingCar.Length - symbolsThatDidntPass;
-                    char symbolHit = passingCar[indexOfHitSymbol];
-                    Crash(passingCar, symbolHit);
-                }
+                input = Console.ReadLine();
             }
 
             Console.WriteLine("Everyone is safe.");
             Console.WriteLine($"{totalCarsPassed} total cars passed the crossroads.");
         }
 
-        private static void Crash(string car, char symbolHit)
+        private static void Crash(string car, char symbolOfHit)
         {
             Console.WriteLine("A crash happened!");
-            Console.WriteLine($"{car} was hit at {symbolHit}.");
+            Console.WriteLine($"{car} was hit at {symbolOfHit}.");
             Environment.Exit(0);
         }
     }

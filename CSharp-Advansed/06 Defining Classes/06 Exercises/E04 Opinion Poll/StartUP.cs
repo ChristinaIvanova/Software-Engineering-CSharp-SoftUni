097c8b565bb6947ae909bcddb59cd513c17b7720
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-19 22:01:25 +0300

    Add solutions

---
+++ CSharp-Advansed/06 Defining Classes/06 Exercises/E04 Opinion Poll/StartUP.cs
@@ -0,0 +1,33 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            var countMember = int.Parse(Console.ReadLine());
+
+            var family = new Family();
+
+            for (int i = 0; i < countMember; i++)
+            {
+                var personArgs = Console.ReadLine().Split();
+
+                var name = personArgs[0];
+                var age = int.Parse(personArgs[1]);
+
+                var person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            var membersOverThirty = family.GetMembersOverThirty();
+            Console.WriteLine(string.Join(Environment.NewLine,membersOverThirty));
+            //foreach (var member in membersOverThirty)
+            //{
+            //    Console.WriteLine(member);
+            //}
+
+        }
+    }
+}

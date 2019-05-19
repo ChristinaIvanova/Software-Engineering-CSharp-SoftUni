3ce545f86ef185e84376bbf5a987334147669c09
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-19 22:03:43 +0300

    Add solutions

---
+++ CSharp-Advansed/06 Defining Classes/06 Exercises/E04 Opinion Poll/Family.cs
@@ -0,0 +1,24 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Family
+    {
+        private List<Person> people { get; set; } = new List<Person>();
+
+        public void AddMember(Person member)
+        {
+            this.people.Add(member);
+        }
+
+        public List<Person> GetMembersOverThirty()
+        {
+            return this.people.Where(x => x.Age >= 30)
+                .OrderBy(x => x.Name)
+                .ToList();
+        }
+    }
+}

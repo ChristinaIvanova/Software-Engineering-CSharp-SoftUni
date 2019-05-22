bf1a052622fcd3e7278a9df4426061407de4d264
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-22 17:16:01 +0300

    Add solutions

---
+++ CSharp-Advansed/02-Multidimensional Arrays/E06 Bomb the Basement/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+
+namespace E06_Bomb_the_Basement
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dimensions = Console.ReadLine()
+                 .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            var rows = dimensions[0];
+            var cols = dimensions[1];
+
+            var matrix = new int[rows][];
+            for (int row = 0; row < rows; row++)
+            {
+                matrix[row] = new int[cols];
+            }
+
+            var bomb = Console.ReadLine()
+                 .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            BombedArray(rows, cols, matrix, bomb);
+
+            FinalBasement(cols, matrix);
+
+            Print(matrix);
+        }
+
+        private static void FinalBasement(int cols, int[][] matrix)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                var currentCol = GetColumn(matrix, col);
+                var countBombedCells = currentCol.Where(x => x == 1).Count();
+
+                if (countBombedCells > 0)
+                {
+                    ClearColumn(matrix, col);
+
+                    for (int i = 0; i < countBombedCells; i++)
+                    {
+                        matrix[i][col] = 1;
+                    }
+                }
+            }
+        }
+
+        private static void BombedArray(int rows, int cols, int[][] matrix, int[] bomb)
+        {
+            var bombRow = bomb[0];
+            var bombCol = bomb[1];
+            var radius = bomb[2];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    bool isInRadius = Math.Pow(row - bombRow, 2) + Math.Pow(col - bombCol, 2) <=
+                        Math.Pow(radius, 2);
+
+                    if (isInRadius)
+                    {
+                        matrix[row][col] = 1;
+                    }
+                }
+            }
+        }
+
+        private static int[] GetColumn(int[][] matrix, int columnNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(0))
+                    .Select(x => matrix[x][columnNumber])
+                    .ToArray();
+        }
+        private static void ClearColumn(int[][] matrix, int columnNumber)
+        {
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrix[row][columnNumber] = 0;
+            }
+        }
+        
+        private static void Print(int[][] matrix)
+        {
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+     
+    }
+
+
+}
---
+++ CSharp-Advansed/02-Multidimensional Arrays/E07 Knight Game secondSolution/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+
+namespace E07_Knight_Game
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[][] board = new char[n][];
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                char[] inputRow = Console.ReadLine().ToCharArray();
+                board[i] = new char[n];
+                board[i] = inputRow;
+            }
+
+            var possibleMoves = new int[]
+            {
+                -2,-1,
+                -2,1,
+                2,-1,
+                2,1,
+                -1,-2,
+                -1,2,
+                1,-2,
+                1,2
+            };
+
+            int currentKnightsInDanger = 0;
+            int maxKnightsInDanger = 0;
+            int mostDangerousKnightRow = 0;
+            int mostDangerousKnightCol = 0;
+            int count = 0;
+
+            while (true)
+            {
+                for (int rowIndex = 0; rowIndex < board.Length; rowIndex++)
+                {
+                    for (int colIndex = 0; colIndex < board[rowIndex].Length; colIndex++)
+                    {
+                        if (board[rowIndex][colIndex].Equals('K'))
+                        {
+                            for (int i = 0; i < possibleMoves.Length; i+=2)
+                            {
+                                currentKnightsInDanger += CountKnightsInDanger(rowIndex + possibleMoves[i], colIndex + possibleMoves[i + 1], board);
+                            }
+                        }
+
+                        if (currentKnightsInDanger > maxKnightsInDanger)
+                        {
+                            maxKnightsInDanger = currentKnightsInDanger;
+                            mostDangerousKnightRow = rowIndex;
+                            mostDangerousKnightCol = colIndex;
+                        }
+                        currentKnightsInDanger = 0;
+                    }
+                }
+                if (maxKnightsInDanger != 0)
+                {
+                    board[mostDangerousKnightRow][mostDangerousKnightCol] = 'O';
+                    count++;
+                    maxKnightsInDanger = 0;
+                }
+                else
+                {
+                    Console.WriteLine(count);
+                    return;
+                }
+            }
+        }
+
+        private static int CountKnightsInDanger(int row, int col,char[][] matrix)
+        {
+            var counKnights = 0;
+
+            if (IsCellInMatrix(row, col, matrix))
+            {
+                if (matrix[row][col].Equals('K'))
+                {
+                    counKnights++;
+                }
+            }
+
+            return counKnights;
+        }
+
+        public static bool IsCellInMatrix(int row, int col, char[][] matrix)
+        {
+            if (0 <= row && row < matrix.Length && 0 <= col && col < matrix[row].Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+

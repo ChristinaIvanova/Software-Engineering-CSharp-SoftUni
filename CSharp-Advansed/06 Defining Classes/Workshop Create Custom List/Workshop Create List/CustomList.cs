bd313b46dcfc4f54fd6212fd65ceefd2188f8ab6
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-23 23:54:44 +0300

    Add solution

---
+++ CSharp-Advansed/06 Defining Classes/Workshop Create Custom List/Workshop Create List/CustomList.cs
@@ -0,0 +1,152 @@
+﻿namespace Workshop_Create_List
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class CustomList
+    {
+        private const int InitialCapacity = 2;
+        private int[] items;
+
+        public CustomList()
+        {
+            this.items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                return items[index];
+            }
+            set
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                items[index] = value;
+            }
+        }
+
+        public void Add(int value)
+        {
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            this.items[this.Count] = value;
+            this.Count++;
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[this.items.Length * 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                copy[i] = this.items[i];
+            }
+
+            this.items = copy;
+        }
+
+        public int RemoveAt(int index)
+        {
+            if (index > this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var item = this.items[index];
+            this.items[index] = default(int);
+            this.Shift(index);
+
+            this.Count--;
+
+            if (this.Count <= this.items.Length / 4)
+            {
+                this.Shrink();
+            }
+
+            return item;
+        }
+
+        private void Shift(int index)
+        {
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+        }
+
+        private void Shrink()
+        {
+            int[] copy = new int[this.items.Length / 2];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                copy[i] = this.items[i];
+            }
+
+            this.items = copy;
+        }
+
+        public void Insert(int index, int value)
+        {
+            if (index > this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            this.ShiftToRight(index);
+            this.items[index] = value;
+            this.Count--;
+        }
+
+        private void ShiftToRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+            {
+                this.items[i] = this.items[i - 1];
+            }
+        }
+
+        public bool Contains(int value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (this.items[i] == value)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            var firstValue = this.items[firstIndex];
+            var secondVaue = this.items[secondIndex];
+
+            this.items[firstIndex] = secondVaue;
+            this.items[secondIndex] = firstValue;
+        }
+    }
+}
---
+++ CSharp-Advansed/06 Defining Classes/Workshop Create Custom List/Workshop Create List/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Workshop_Create_List
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var listTest = new CustomList();
+
+            listTest.Add(2);
+            listTest.Add(3);
+            listTest.Add(5);
+            listTest.Add(6);
+            //2 3 5
+            Console.WriteLine(listTest.Count == 4);
+
+            listTest.RemoveAt(2);
+            // 2 3 6
+            Console.WriteLine(listTest[2] == 6);
+
+            listTest.Add(4);
+            listTest.Add(42);
+            listTest.Add(41);
+            listTest.RemoveAt(0);
+            //3 6 4 42 41
+            Console.WriteLine(listTest[0] == 3);
+            Console.WriteLine(listTest[1] == 6);
+  
+            Console.WriteLine(listTest[listTest.Count - 1] == 41);
+            Console.WriteLine(listTest[listTest.Count - 2] == 42);
+
+            listTest.RemoveAt(2);
+            listTest.RemoveAt(2);
+
+        }
+    }
+}

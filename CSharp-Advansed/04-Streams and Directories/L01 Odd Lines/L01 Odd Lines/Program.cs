f42a02f0d494ec21aa23fd9b934e18f44fe06d9c
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-30 10:22:09 +0300

    Add solution

---
+++ CSharp-Advansed/04-Streams and Directories/L01 Odd Lines/L01 Odd Lines/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace L01_Odd_Lines
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main()
+        {
+            using (var reader = new StreamReader("Input.txt"))
+            {
+                using (var writer = new StreamWriter("Output.txt"))
+                {
+                    int counter = 0;
+
+                    while (true)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        if (counter % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}

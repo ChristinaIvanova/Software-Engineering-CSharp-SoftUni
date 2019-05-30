bbc0566cfb1e79f26ea9e65ccee24e6630a80181
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-30 10:47:11 +0300

    Add solution

---
+++ CSharp-Advansed/04-Streams and Directories/L02 Line Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace L02_Line_Numbers
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main()
+        {
+            using (var reader = new StreamReader("Input.txt"))
+            {
+                using (var writer = new StreamWriter("Output.txt"))
+                {
+                    var countLines = 1;
+
+                    while (true)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        line = $"{countLines}. {line}";
+                        writer.WriteLine(line);
+
+                        countLines++;
+                    }
+                }
+            }
+        }
+    }
+}

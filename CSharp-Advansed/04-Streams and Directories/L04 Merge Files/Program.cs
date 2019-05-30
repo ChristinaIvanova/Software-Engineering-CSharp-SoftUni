bf7e626de773dd97515e5c7660f8088e31988f34
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-30 15:09:05 +0300

    Add solution

---
+++ CSharp-Advansed/04-Streams and Directories/L04 Merge Files/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace L04_Merge_Files
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] linesFirstFile = File.ReadAllLines("FileOne.txt");
+            string[] linesSecondFile = File.ReadAllLines("FileTwo.txt");
+
+            var resultText = new List<string>();
+
+            var maxLenght = Math.Max(linesFirstFile.Length,linesSecondFile.Length);
+            
+            for (int i = 0; i < maxLenght; i++)
+            {
+                if (linesFirstFile.Length <= maxLenght)
+                {
+                    resultText.Add(linesFirstFile[i]);
+                }
+                if (linesSecondFile.Length <= maxLenght)
+                {
+                    resultText.Add(linesSecondFile[i]);
+                }
+            }
+
+            File.WriteAllLines("MergedFile.txt", resultText);
+        }
+    }
+}

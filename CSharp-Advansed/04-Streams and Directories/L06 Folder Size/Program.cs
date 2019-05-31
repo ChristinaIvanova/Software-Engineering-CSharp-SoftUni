7ebb12fbc2f7effa14abe910c54a91d469381dee
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-05-31 10:56:26 +0300

    Add solution

---
+++ CSharp-Advansed/04-Streams and Directories/L06 Folder Size/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace L06_Folder_Size
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] filesInDir = Directory.GetFiles("../../../TestFolder");
+
+            double totalSize = 0;
+
+            foreach (string file in filesInDir)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                totalSize += fileInfo.Length;
+            }
+
+            totalSize = totalSize / 1024 / 1024;
+
+            File.WriteAllText("output.txt", totalSize.ToString());
+        }
+    }
+}

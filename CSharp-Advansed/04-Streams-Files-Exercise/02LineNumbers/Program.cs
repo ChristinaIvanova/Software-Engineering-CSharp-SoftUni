7dc708ab0bb0799b766944e5a8eade720d67da9f
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-06 18:28:34 +0300

    Add solution

---
+++ CSharp-Advansed/04-Streams-Files-Exercise/02LineNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var textPath = "../../../text.txt";
+            var outputPath = "../../../output.txt";
+
+            var text = File.ReadAllLines(textPath).ToArray();
+            
+            for (int i = 0; i < text.Length; i++)
+            {
+                var line = text[i];
+                
+                var countLetters = line.Count(char.IsLetter);
+
+                var countPunctuations = line.Count(char.IsPunctuation);
+
+                File.AppendAllText(outputPath, $"Line {i + 1}: {line} ({countLetters})({countPunctuations}){Environment.NewLine}");
+            }
+        }
+    }
+}

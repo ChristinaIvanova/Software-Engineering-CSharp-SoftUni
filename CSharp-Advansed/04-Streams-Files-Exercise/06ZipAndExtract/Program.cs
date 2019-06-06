19ae4afd9b23990c32651929a5b2fcbbadfc9cc5
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-06 18:31:00 +0300

    Add solution

---
+++ CSharp-Advansed/04-Streams-Files-Exercise/06ZipAndExtract/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06ZipAndExtract
+{
+    using System;
+    using System.IO.Compression;
+
+    class Program
+    {
+        static void Main()
+        {
+            var fileFolderPath = "./";
+            var targetPath = "../../../result.zip";
+
+            ZipFile.CreateFromDirectory(fileFolderPath, targetPath);
+
+            ZipFile.ExtractToDirectory(targetPath, "../../../extractedZip");
+        }
+    }
+}

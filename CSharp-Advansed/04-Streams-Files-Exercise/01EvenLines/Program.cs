9717433aaccb928347a420a7c3e7acec49681f5b
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-06 18:28:01 +0300

    Add solution

---
+++ CSharp-Advansed/04-Streams-Files-Exercise/01EvenLines/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _01EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var specialSymbols = "-.,!?";
+            var counter = 0;
+
+            using (var reader = new StreamReader("../../../text.txt"))
+            {
+                using (var writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (true)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        if (counter % 2 == 0)
+                        {
+                            var changedLine = string.Empty;
+
+                            foreach (var character in line)
+                            {
+                                if (specialSymbols.Contains(character))
+                                {
+                                    changedLine += "@";
+                                }
+                                else
+                                {
+                                    changedLine += character;
+                                }
+                            }
+
+                            var splittedLine = changedLine.Split().ToArray();
+
+                            Array.Reverse(splittedLine);
+
+                            writer.WriteLine(string.Join(" ", splittedLine));
+                        }
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}

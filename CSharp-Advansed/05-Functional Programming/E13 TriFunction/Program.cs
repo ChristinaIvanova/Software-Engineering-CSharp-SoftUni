1875f4ef129c044c7450fd168e9d2453229ca6fc
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-07 22:51:24 +0300

    Add solution

---
+++ CSharp-Advansed/05-Functional Programming/E13 TriFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace E13_TriFunction
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var length = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine()
+                .Split();
+
+            Func<string, int, bool> isLarger = (name, charsSum)
+                => name.Sum(x => x) >= charsSum;
+
+            Func<string[], Func<string, int, bool>, string> nameFilter =
+                (inputNames, isLargerFilter)
+                => inputNames.FirstOrDefault(x => isLargerFilter(x, length));
+
+            var result = nameFilter(names, isLarger);
+            Console.WriteLine(result);
+        }
+    }
+}

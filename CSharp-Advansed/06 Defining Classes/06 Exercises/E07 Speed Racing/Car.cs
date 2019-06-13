38eee63a6f9bbb92b2aedec4fe9211ee429a6299
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-13 23:19:00 +0300

    Add solution

---
+++ CSharp-Advansed/06 Defining Classes/06 Exercises/E07 Speed Racing/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E07_Speed_Racing
+{
+    class Car
+    {
+        private string model;
+        private double fuel;
+        private double fuelConsumption;
+        private int traveledDistance;
+
+        public Car(string model, double fuel, double fuelConsumption)
+        {
+            this.Model = model;
+            this.Fuel = fuel;
+            this.FuelConsumption = fuelConsumption;
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public double Fuel
+        {
+            get { return fuel; }
+            set { fuel = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public int Distance
+        {
+            get { return traveledDistance; }
+            set { traveledDistance = value; }
+        }
+
+        public bool CanGoOnTravel(int distance)
+        {
+            if (FuelConsumption * distance <= Fuel)
+            {
+                Distance += distance;
+                Fuel-=FuelConsumption*distance;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{model} {Fuel:f2} {Distance}"; 
+        }
+    }
+}
---
+++ CSharp-Advansed/06 Defining Classes/06 Exercises/E07 Speed Racing/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace E07_Speed_Racing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var cars = new List<Car>();
+            var countCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countCars; i++)
+            {
+                var carArgs = Console.ReadLine()
+                    .Split();
+
+                var model = carArgs[0];
+                var fuel = double.Parse(carArgs[1]);
+                var fuelConsumption = double.Parse(carArgs[2]);
+
+                var car = new Car(model, fuel, fuelConsumption);
+                cars.Add(car);
+            }
+
+            var input = Console.ReadLine();
+
+            while (input!="End")
+            {
+                var tokens = input.Split();
+
+                var model = tokens[1];
+                var distance = int.Parse(tokens[2]);
+
+                var car = cars.FirstOrDefault(x => x.Model == model);
+
+                if (!car.CanGoOnTravel(distance))
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            cars.ForEach(Console.WriteLine);
+        }
+    }
+}

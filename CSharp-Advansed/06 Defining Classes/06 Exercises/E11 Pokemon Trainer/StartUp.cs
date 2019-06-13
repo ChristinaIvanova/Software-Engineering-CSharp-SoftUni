1db6c52d1bdaaad51c8751309edca30e3e335fd7
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-13 23:21:05 +0300

    Add solution

---
+++ CSharp-Advansed/06 Defining Classes/06 Exercises/E11 Pokemon Trainer/StartUp.cs
@@ -0,0 +1,65 @@
+﻿namespace PokemonTrainer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            var trainers = new List<Trainer>();
+
+            var input = Console.ReadLine();
+
+            while (input != "Tournament")
+            {
+                var tokens = input
+                    .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var trainerName = tokens[0];
+                var pokemonName = tokens[1];
+                var element = tokens[2];
+                var health = int.Parse(tokens[3]);
+
+                var pokemon = new Pokemon(pokemonName, element, health);
+
+                if (!trainers.Any(t => t.Name == trainerName))
+                {
+                    trainers.Add(new Trainer(trainerName));
+                }
+
+                Trainer trainer = trainers.FirstOrDefault(t => t.Name == trainerName);
+                trainer.Pokemons.Add(pokemon);
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input!="End")
+            {
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.Pokemons.Any(p=>p.Element==input))
+                    {
+                        trainer.IncreaseBadges();
+                    }
+                    else
+                    {
+                        trainer.ReducePokemonsHealth();
+                        trainer.RemoveDead();
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            trainers
+                .OrderByDescending(t => t.Badges)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ CSharp-Advansed/06 Defining Classes/06 Exercises/E11 Pokemon Trainer/Trainer.cs
@@ -0,0 +1,43 @@
+﻿namespace PokemonTrainer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+       
+        public int Badges { get; set; }
+
+        public List<Pokemon> Pokemons { get; private set; } = new List<Pokemon>();
+        
+        public void IncreaseBadges()
+        {
+            Badges++;
+        }
+
+        public void ReducePokemonsHealth()
+        {
+            this.Pokemons.ForEach(p => p.ReduceHealth());
+        }
+
+        public void RemoveDead()
+        {
+            this.Pokemons = this.Pokemons
+                .Where(p => p.Health > 0)
+                .ToList();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Badges} {Pokemons.Count}";
+        }
+    }
+}

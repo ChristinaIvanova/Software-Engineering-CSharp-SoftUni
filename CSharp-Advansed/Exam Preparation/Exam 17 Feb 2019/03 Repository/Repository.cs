7189df588eb8246cb6d69d2867afc574576b414d
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-15 14:19:47 +0300

    Add solution

---
+++ CSharp-Advansed/Exam Preparation/Exam 17 Feb 2019/03 Repository/Repository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class Repository
+    {
+        private Dictionary<int, Person> data;
+        private int id;
+
+        public Repository()
+        {
+            this.data = new Dictionary<int, Person>();
+        }
+
+        public int Count
+        {
+            get
+            {
+              return this.data.Count;
+            }
+        }
+
+        public void Add(Person person)
+        {
+            this.data.Add(id, person);
+            id++;
+        }
+
+        public Person Get(int id)
+        {
+            if (this.data.ContainsKey(id))
+            {
+                return this.data[id];
+            }
+
+            return null;
+        }
+
+        public bool Update(int id, Person newPerson)
+        {
+            if (this.data.ContainsKey(id))
+            {
+                this.data[id] = newPerson;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Delete(int id)
+        {
+            if (this.data.ContainsKey(id))
+            {
+                this.data.Remove(id);
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+}

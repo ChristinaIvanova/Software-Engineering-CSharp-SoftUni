6052e9c7244f9c87510b452fc269cf6ea089f011
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-17 14:56:28 +0300

    Add solution

---
+++ CSharp-Advansed/07-Generics/Generic Exercises/E02 Generic Box of Integer/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace E02_Generic_Box_of_Integer
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                var conten = int.Parse(Console.ReadLine());
+
+                var boxOfInteger = new Box<int>(conten);
+                Console.WriteLine(boxOfInteger);
+            }
+        }
+    }
+}

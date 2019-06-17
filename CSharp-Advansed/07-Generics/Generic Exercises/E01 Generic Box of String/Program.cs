7ffe041a471f2c5acdbbb049a171f4e56f1a01fd
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-17 14:56:08 +0300

    Add solution

---
+++ CSharp-Advansed/07-Generics/Generic Exercises/E01 Generic Box of String/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace E01_Generic_Box_of_String
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                var content = Console.ReadLine();
+
+                var boxOfString = new Box<string>(content);
+                Console.WriteLine(boxOfString);
+            }
+        }
+    }
+}

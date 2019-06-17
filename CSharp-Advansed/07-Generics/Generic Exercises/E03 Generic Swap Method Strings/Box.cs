fd3332c8b890a3b0b0943fd0eb84fdca1a56f2c7
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-17 14:56:42 +0300

    Add solution

---
+++ CSharp-Advansed/07-Generics/Generic Exercises/E03 Generic Swap Method Strings/Box.cs
@@ -0,0 +1,42 @@
+﻿namespace E03_Generic_Swap_Method_Strings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Box<T>
+    {
+        private List<T> values;
+
+        public Box()
+        {
+            this.values = new List<T>();
+        }
+
+        public List<T> Values => this.values;
+
+        public void Add(T item)
+        {
+            this.values.Add(item);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = this.values[firstIndex];
+            this.values[firstIndex] = this.values[secondIndex];
+            this.values[secondIndex] = temp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in values)
+            {
+                sb.AppendLine($"{typeof(T)} {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp-Advansed/07-Generics/Generic Exercises/E03 Generic Swap Method Strings/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace E03_Generic_Swap_Method_Strings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var linesCount = int.Parse(Console.ReadLine());
+
+            var box = new Box<string>();
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                var line = Console.ReadLine();
+                box.Add(line);
+            }
+
+            var indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var firstIndex = indexes[0];
+            var secondIndex = indexes[1];
+
+            box.Swap(firstIndex, secondIndex);
+
+           // Swap(box.Values, firstIndex, secondIndex);
+
+            Console.WriteLine(box);
+        }
+
+        static void Swap<T>(List<T> listWithValues, int firstIndex, int secondIndex)
+        {
+            var temp = listWithValues[firstIndex];
+            listWithValues[firstIndex] = listWithValues[secondIndex];
+            listWithValues[secondIndex] = temp;
+        }
+    }
+}

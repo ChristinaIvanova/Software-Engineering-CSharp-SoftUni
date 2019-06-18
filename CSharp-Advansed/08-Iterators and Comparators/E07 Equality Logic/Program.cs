6244ec87da74e7c3d1f09d1c02440230f31f2389
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-18 23:26:01 +0300

    Add solution

---
+++ CSharp-Advansed/08-Iterators and Comparators/E07 Equality Logic/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace E07EqualityLogic
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+
+            var hashPeople = new HashSet<Person>();
+            var sortedPeople = new SortedSet<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var personArgs = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                var name = personArgs[0];
+                var age = int.Parse(personArgs[1]);
+
+                var person = new Person(name, age);
+
+                hashPeople.Add(person);
+                sortedPeople.Add(person);
+            }
+
+            Console.WriteLine(hashPeople.Count);
+            Console.WriteLine(sortedPeople.Count);
+        }
+    }
+}

c137983e2f326d65b5dd7a49d33d9d207fc9b0ad
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-19 16:00:12 +0300

    Add solution

---
+++ CSharp-Advansed/Exam Preparation/Exam 16 Apr 2019/01 Trojan 2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Trojan_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var waves = int.Parse(Console.ReadLine());
+
+            var plates = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            
+            var warriors = new Stack<int>();
+
+            for (int i = 1; i <= waves; i++)
+            {
+                var rocksInput = Console.ReadLine()
+                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                   .Select(int.Parse)
+                   .ToList();
+
+                if (i % 3 == 0)
+                {
+                    var additionalWall = int.Parse(Console.ReadLine());
+                    plates.Add(additionalWall);
+                }
+
+                rocksInput.ForEach(x => warriors.Push(x));
+
+                while (plates.Any() && warriors.Any())
+                {
+                    var currentRock = warriors.Pop();
+                    var currentWall = plates[0];
+
+                    if (currentRock > currentWall)
+                    {
+                        warriors.Push(currentRock - currentWall);
+                        plates.RemoveAt(0);
+                    }
+                    else if (currentRock == currentWall)
+                    {
+                        plates.RemoveAt(0);
+                    }
+                    else if (currentRock < currentWall)
+                    {
+                        plates[0]=currentWall - currentRock;
+                    }
+                }
+
+                if (plates.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            if (warriors.Any())
+            {
+                var leftRocks = string.Join(", ", warriors);
+
+                Console.WriteLine($"The Trojans successfully destroyed the Spartan defense.");
+                Console.WriteLine($"Warriors left: {leftRocks}");
+            }
+            else
+            {
+                var leftWalls = string.Join(", ", plates);
+                Console.WriteLine($"The Spartans successfully repulsed the Trojan attack.");
+                Console.WriteLine($"Plates left: {leftWalls}");
+            }
+        }
+    }
+}
---
+++ CSharp-Advansed/Exam Preparation/Exam 16 Apr 2019/01 Trojan Inavasion/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Trojan_Inavasion
+{
+    class Program
+    {
+        static void Main()
+        {
+            var wavesCount = int.Parse(Console.ReadLine());
+
+            var platesArgs = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+
+            var plates = new Queue<int>(platesArgs);
+            var warriors = new Stack<int>();
+
+
+
+            for (int i = 1; i <= wavesCount; i++)
+            {
+                var input = Console.ReadLine();
+
+                if (i % 3 == 0)
+                {
+                    var additionalPlate = Console.ReadLine();
+                    plates.Enqueue(int.Parse(additionalPlate));
+                }
+
+                var wave = input
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+
+                wave.ToList().ForEach(warriors.Push);
+
+                var plate = plates.Peek();
+                var warrior = 0;
+
+                while (plate > 0 && warriors.Any())
+                {
+                    warrior = warriors.Pop();
+                    plate -= warrior;
+                }
+
+                plates.Dequeue();
+
+                if (plate < 0)
+                {
+                    warriors.Push(Math.Abs(plate));
+                }
+                
+                if (plates.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            if (warriors.Any())
+            {
+
+                var leftWarriors = string.Join(", ", warriors);
+
+                Console.WriteLine($"The Trojans successfully destroyed the Spartan defense.");
+                Console.WriteLine($"Warriors left: {leftWarriors}");
+            }
+            else
+            {
+                var leftPlates = string.Join(", ", plates);
+
+                Console.WriteLine($"The Spartans successfully repulsed the Trojan attack.");
+                Console.WriteLine($"Plates left: {leftPlates}");
+            }
+        }
+    }
+}

fc349095575679fb9eb37469a804fd7809714fcc
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-27 23:30:08 +0300

    Add solution

---
+++ CSharp-Advansed/Exam Preparation/Exam 17 Dec 2018/01 Sport Cards/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Sport_Cards
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sportCardsInfo = new Dictionary<string, Dictionary<string, double>>();
+
+            var input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                if (input.Split(" ").Length != 2)
+                {
+                    var commandArgs = input
+                       .Split(new[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
+
+                    var cardName = commandArgs[0];
+                    var sport = commandArgs[1];
+                    var price = double.Parse(commandArgs[2]);
+
+                    if (!sportCardsInfo.ContainsKey(cardName))
+                    {
+                        sportCardsInfo[cardName] = new Dictionary<string, double>();
+                    }
+
+                    sportCardsInfo[cardName][sport] = price;
+                }
+                else
+                {
+                    var commandArgs = input
+                       .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    var cardName = commandArgs[1];
+
+                    if (sportCardsInfo.ContainsKey(cardName))
+                    {
+                        Console.WriteLine($"{cardName} is available!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{cardName} is not available!");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var kvp in sportCardsInfo
+                .OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"{kvp.Key}:");
+
+                Console.WriteLine(string.Join
+                    (Environment.NewLine, kvp.Value.OrderBy(x => x.Key)
+                    .Select(s => $" -{s.Key} - {s.Value:f2}")));
+            }
+        }
+    }
+}
---
+++ CSharp-Advansed/Exam Preparation/Exam 17 Dec 2018/04 The Kitchen/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_The_Kitchen
+{
+    class Program
+    {
+        static void Main()
+        {
+            var knivesInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+
+            var forksInput = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse);
+
+            var knives = new Stack<int>(knivesInput);
+            var forks = new Queue<int>(forksInput);
+
+            var readyPairs = new List<int>();
+
+            while (knives.Any() && forks.Any())
+            {
+                var currentKnife = knives.Pop();
+                var currentFork = forks.Peek();
+
+                if (currentKnife > currentFork)
+                {
+                    forks.Dequeue();
+                    readyPairs.Add(currentKnife + currentFork);
+                }
+                else if (currentKnife == currentFork)
+                {
+                    forks.Dequeue();
+                    knives.Push(currentKnife + 1);
+                }
+            }
+
+            var biggestSet = readyPairs.Max();
+            var pairs = string.Join(" ", readyPairs);
+
+            Console.WriteLine($"The biggest set is: {biggestSet}");
+            Console.WriteLine(pairs);
+        }
+    }
+}

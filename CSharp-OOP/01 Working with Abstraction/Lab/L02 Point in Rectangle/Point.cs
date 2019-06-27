96890257ce7f3d6706ebfbb55555438e940c7769
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-27 23:12:32 +0300

    Add solution

---
+++ CSharp-OOP/01 Working with Abstraction/Lab/L02 Point in Rectangle/Point.cs
@@ -0,0 +1,19 @@
+﻿namespace L02PointInRectangle
+{
+  public  class Point
+    {
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+    }
+}
---
+++ CSharp-OOP/01 Working with Abstraction/Lab/L02 Point in Rectangle/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace L02PointInRectangle
+{
+  public  class Startup
+    {
+        static void Main()
+        {
+            var coordinatesRectangle = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var topLeft = new Point(coordinatesRectangle[0], coordinatesRectangle[1]);
+            var bottomRight = new Point(coordinatesRectangle[2], coordinatesRectangle[3]);
+
+            var rectangle = new Rectangle(topLeft, bottomRight);
+
+            var lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                var pointArgs= Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                var pointToCheck = new Point(pointArgs[0], pointArgs[1]);
+
+                Console.WriteLine(rectangle.Contains(pointToCheck));
+            }
+        }
+    }
+}

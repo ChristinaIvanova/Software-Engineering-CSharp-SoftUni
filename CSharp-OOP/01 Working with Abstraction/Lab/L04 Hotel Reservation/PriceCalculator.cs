b52b78c2421f0d971644c4eba44bb6780d29e416
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-06-27 23:13:02 +0300

    Add solution

---
+++ CSharp-OOP/01 Working with Abstraction/Lab/L04 Hotel Reservation/PriceCalculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L04_Hotel_Reservation
+{
+    class PriceCalculator
+    {
+    }
+}
---
+++ CSharp-OOP/01 Working with Abstraction/Lab/L04 Hotel Reservation/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L04HotelReservation
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var priceCalculator = new PriceCalculator();
+
+            var reservationArgs = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var pricePerDay = decimal.Parse(reservationArgs[0]);
+            var days = int.Parse(reservationArgs[1]);
+            var season 
+            var discount = reservationArgs[3];
+
+            if (season==Season.Spring)
+            {
+
+            }
+            priceCalculator.CalculatePrice(pricePerDay, days, season, discount);
+        }
+    }
+}

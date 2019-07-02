78d419d69e38422b81bdcf31c3d533909b47423a
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-07-02 23:49:08 +0300

    Add solution

---
+++ CSharp-OOP/01 Working with Abstraction/Exercises/P02_CarsSalesman/CarSalesman.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P02_CarsSalesman
+{
+    public class CarSalesman
+    {
+        private CarFactory carFactory;
+        private EngineFactory engineFactory;
+        private List<Car> cars;
+        private List<Engine> engines;
+
+        public CarSalesman(CarFactory carFactory, EngineFactory engineFactory)
+        {
+            this.carFactory = carFactory;
+            this.engineFactory = engineFactory;
+            this.cars = new List<Car>();
+            this.engines = new List<Engine>();
+        }
+
+        public void AddEngine(string[] parameters)
+        {
+            Engine engine = this.engineFactory.Create(parameters);
+
+            this.engines.Add(engine);
+        }
+
+        public void AddCar(string[] parameters)
+        {
+            Car car = this.carFactory.Create(parameters, this.engines);
+
+            this.cars.Add(car);
+        }
+
+        public List<Car> GetCars()
+        {
+            return this.cars;
+        }
+    }
+}
---
+++ CSharp-OOP/01 Working with Abstraction/Exercises/P02_CarsSalesman/Engine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02_CarsSalesman
+{
+    public class Engine
+    {
+        private int power;
+        private int displacement;
+        private string efficiency;
+
+        public Engine(string model, int power, int displacement, string efficiency)
+        {
+            this.Model = model;
+            this.power = power;
+            this.displacement = displacement;
+            this.efficiency = efficiency;
+        }
+
+        public Engine(string model, int power)
+            : this(model, power, -1, "n/a")
+        {
+        }
+
+        public Engine(string model, int power, int displacement)
+            : this(model, power, displacement, "n/a")
+        {
+        }
+
+        public Engine(string model, int power, string efficiency)
+            : this(model, power, -1, efficiency)
+        {
+        }
+
+        public string Model { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"  {this.Model}:");
+            sb.AppendLine($"    Power: {this.power}");
+            sb.AppendLine($"    Displacement: {(this.displacement == -1 ? "n/a" : this.displacement.ToString())}");
+            sb.AppendLine($"    Efficiency: {this.efficiency}");
+
+            return sb.ToString();
+        }
+    }
+}

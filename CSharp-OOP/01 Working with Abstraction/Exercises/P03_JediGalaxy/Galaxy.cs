ce08ed29961a8634ed3fd7c39c5ab8473ba5fe60
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-07-02 23:50:24 +0300

    Add solution

---
+++ CSharp-OOP/01 Working with Abstraction/Exercises/P03_JediGalaxy/Galaxy.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_JediGalaxy
+{
+    public class Galaxy
+    {
+        private int[,] matrix;
+        private int row;
+        private int col;
+
+        public Galaxy(int row, int col)
+        {
+            this.row = row;
+            this.col = col; 
+            this.matrix = new int[this.row, this.col];
+        }
+
+        public void CreateStars()
+        {
+            int value = 0;
+
+            for (int row = 0; row < this.row; row++)
+            {
+                for (int j = 0; j < this.col; j++)
+                {
+                   this.matrix[row, j] = value++;
+                }
+            }
+        }
+
+        public void ClearStars(int[] coordinates)
+        {
+            int row = coordinates[0];
+            int col = coordinates[1];
+
+            while (row >= 0 && col >= 0)
+            {
+                if (row >= 0 && row < this.matrix.GetLength(0)
+                    && col >= 0 && col < this.matrix.GetLength(1))
+                {
+                    this.matrix[row, col] = 0;
+                }
+                row--;
+                col--;
+            }
+        }
+
+        public long SelectStars(int[] coordinates)
+        {
+            int row = coordinates[0];
+            int col = coordinates[1];
+
+            long sum = 0; 
+
+            while (row >= 0 && col < this.matrix.GetLength(1))
+            {
+                if (row >= 0 && row < this.matrix.GetLength(0) 
+                    && col >= 0 && col < this.matrix.GetLength(1))
+                {
+                    sum += this.matrix[row, col];
+                }
+
+                col++;
+                row--;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ CSharp-OOP/01 Working with Abstraction/Exercises/P03_JediGalaxy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_JediGalaxy
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            Galaxy galaxy = new Galaxy(rows, cols);
+            galaxy.CreateStars();
+
+            long sumOfStars = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "Let the Force be with you")
+            {
+                int[] playerCoordinates = command
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int[] evilCoordinates = Console.ReadLine()
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                galaxy.ClearStars(evilCoordinates);
+                sumOfStars += galaxy.SelectStars(playerCoordinates);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(sumOfStars);
+        }
+    }
+}

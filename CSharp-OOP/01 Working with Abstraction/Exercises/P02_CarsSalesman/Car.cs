d1b833d8df7c18c182b429dc81d6c44aca417883
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-07-02 23:49:41 +0300

    Add solution

---
+++ CSharp-OOP/01 Working with Abstraction/Exercises/P02_CarsSalesman/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02_CarsSalesman
+{
+    public class Car
+    {
+        private string model;
+        private Engine engine;
+        private int weight;
+        private string color;
+
+        public Car(string model, Engine engine, int weight, string color)
+        {
+            this.model = model;
+            this.engine = engine;
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public Car(string model, Engine engine)
+                   : this(model, engine, -1, "n/a")
+        {
+        }
+
+        public Car(string model, Engine engine, int weight)
+        : this(model, engine, weight, "n/a")
+        {
+        }
+
+        public Car(string model, Engine engine, string color)
+            : this(model, engine, -1, color)
+        {
+        }
+        
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.model}:");
+            sb.Append(this.engine);
+            sb.AppendLine($"  Weight: {(this.weight == -1 ? "n/a" : this.weight.ToString())}");
+            sb.Append($"  Color: {this.color}");
+
+            return sb.ToString();
+        }
+    }
+}
+

ad6a2a1acf3c23a7c5e089be49e1ed2ebc573310
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-07-07 00:05:32 +0300

    Add solution

---
+++ CSharp-OOP/02 Inheritance/Excercises/Zoo/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var animals = new List<Animal>();
+
+            animals.Add(new Bear("Yoggi"));
+            animals.Add(new Mammal("Ruspin"));
+            animals.Add(new Reptile("snake"));
+        }
+    }
+}

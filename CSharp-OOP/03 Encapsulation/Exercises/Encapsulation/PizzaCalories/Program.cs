40aaa6b654b84b0e0293505595d21173c5544d45
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-07-11 23:57:25 +0300

    Add solution

---
+++ CSharp-OOP/03 Encapsulation/Exercises/Encapsulation/PizzaCalories/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var pizzaArgs = Console.ReadLine().Split();
+
+                var pizzaName = pizzaArgs[1];
+
+                var doughArgs = Console.ReadLine().Split();
+
+                var type = doughArgs[1];
+                var baking = doughArgs[2];
+                var grams = double.Parse(doughArgs[3]);
+
+                var dough = new Dough(type, baking, grams);
+
+                var pizza = new Pizza(pizzaName, dough);
+
+                var input = Console.ReadLine();
+
+                while (input != "END")
+                {
+                    var toppingArgs = input.Split();
+                    var typeTopping = toppingArgs[1];
+                    var weight = double.Parse(toppingArgs[2]);
+
+                    var topping = new Topping(typeTopping, weight);
+
+                    pizza.AddTopping(topping);
+
+                    input = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{pizza.Name} - {pizza.GetTotalCalories():f2} Calories.");
+
+            }
+            catch (Exception msg)
+            {
+                Console.WriteLine(msg.Message);
+            }
+
+        }
+    }
+}

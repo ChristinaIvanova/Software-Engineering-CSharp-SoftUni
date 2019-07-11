194b913395a76d150129c3eecd7a38c1faad165d
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-07-11 23:56:50 +0300

    Add solution

---
+++ CSharp-OOP/03 Encapsulation/Exercises/Encapsulation/01ClassBox/Box.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01ClassBox
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                ValidateParameter(value, nameof(this.Length));
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                ValidateParameter(value, nameof(this.Width));
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                ValidateParameter(value, nameof(this.Height));
+
+                this.height = value;
+            }
+        }
+
+        public double GetLateralSurfaceArea()
+        {
+            return 2 * (this.length * this.height + this.width * this.height);
+        }
+
+        public double GetVolume()
+        {
+            return this.length * this.height * this.width;
+        }
+
+        public double GetSurfaceArea()
+        {
+            return 2 * (this.length * this.width) + GetLateralSurfaceArea();
+        }
+
+        private void ValidateParameter(double value, string parameter)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{parameter} cannot be zero or negative.");
+            }
+        }
+    }
+}
---
+++ CSharp-OOP/03 Encapsulation/Exercises/Encapsulation/01ClassBox/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01ClassBox
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var length = double.Parse(Console.ReadLine());
+                var width = double.Parse(Console.ReadLine());
+                var height = double.Parse(Console.ReadLine());
+
+                var box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.GetSurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.GetLateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.GetVolume():f2}");
+            }
+            catch (ArgumentException msg)
+            {
+                Console.WriteLine(msg.Message);
+            }
+
+        }
+    }
+}

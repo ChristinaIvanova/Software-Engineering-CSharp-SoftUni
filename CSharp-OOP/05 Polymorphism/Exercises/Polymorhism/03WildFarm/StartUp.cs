e3b90bb255c852104776b66ce7795f952fee4783
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-08-06 23:44:06 +0300

    Add solution

---
+++ CSharp-OOP/05 Polymorphism/Exercises/Polymorhism/03WildFarm/StartUp.cs
@@ -0,0 +1,15 @@
+﻿
+using _03WildFarm.Core;
+using System;
+
+namespace _03WildFarm
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var engine = new Engine();
+            engine.Run();
+        }
+    }
+}

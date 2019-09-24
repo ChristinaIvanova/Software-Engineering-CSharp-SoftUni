fe97ba16f9508ecafd3a2646b0f454785141319c
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-09-24 23:11:56 +0300

    Add solutions

---
+++ CSharp-OOP/07 Reflection/Exercises/Reflection-and-Attributes-Skeleton/Reflection-and-Attributes-Skeleton/ValidationAttributes/Entities/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Entities
+{
+    public class Person
+    {
+        private const int MinValue = 12;
+        private const int MaxValue = 90;
+
+        public Person(string fullName, int age)
+        {
+            this.FullName = fullName;
+            this.Age = age;
+        }
+
+        [MyRequired]
+        public string FullName { get; private set; }
+
+        [MyRangeAttribute(MinValue, MaxValue)]
+        public int Age { get; private set; }
+    }
+}

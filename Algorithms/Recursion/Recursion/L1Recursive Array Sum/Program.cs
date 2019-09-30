f74be693319d2d5f73c8660f2ce4891128105eeb
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-09-30 23:43:39 +0300

    Add solution

---
+++ Algorithms/Recursion/Recursion/L1Recursive Array Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L1Recursive_Array_Sum
+{
+    class Program
+    {
+        static int Sum(int[] arr, int startIndex)
+        {
+            if (startIndex == arr.Length)
+            {
+                return 0;
+            }
+
+            var currentSum= arr[startIndex] + Sum(arr, startIndex + 1);
+            return currentSum;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 2, 3, 4, 5 };
+
+            var result = Sum(numbers, 0);
+            Console.WriteLine(result);
+        }
+    }
+}

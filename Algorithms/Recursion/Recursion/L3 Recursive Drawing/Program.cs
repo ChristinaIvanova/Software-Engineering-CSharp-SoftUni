a8b0fa94041b5ff243a015c34bc4d3487a1cd025
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-09-30 23:44:53 +0300

    Add solution

---
+++ Algorithms/Recursion/Recursion/L3 Recursive Drawing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace L3_Recursive_Drawing
+{
+    class Program
+    {
+        static void Print(int num)
+        {
+            if (num == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', num));
+
+            Print(num - 1);
+
+            Console.WriteLine(new string('#', num));
+        }
+
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Print(number);
+        }
+    }
+}

b8d8dab68202a19ce3c16a042e7cef22d41abc52
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-09-30 23:44:10 +0300

    Add solution

---
+++ Algorithms/Recursion/Recursion/L2Recursive Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L2Recursive_Factorial
+{
+    class Program
+    {
+        static long Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+
+            var currentResult = num * Factorial(num - 1);
+            return currentResult;
+        }
+
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            var result = Factorial(num);
+
+            Console.WriteLine(result);
+        }
+    }
+}

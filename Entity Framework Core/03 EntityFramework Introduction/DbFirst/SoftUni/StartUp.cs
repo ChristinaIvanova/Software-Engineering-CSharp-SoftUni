f4b5e37c14b969db9dfb9a1a6ad89defa6d7fc1b
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-04 00:47:58 +0200

    Add solutions

--- Entity Framework Core/03 EntityFramework Introduction/DbFirst/SoftUni/StartUp.cs
+++ Entity Framework Core/03 EntityFramework Introduction/DbFirst/SoftUni/StartUp.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using Remotion.Linq.Clauses;
 using SoftUni.Data;
+using SoftUni.Models;
 
 namespace SoftUni
 {
@@ -11,7 +15,7 @@ namespace SoftUni
         {
             var context = new SoftUniContext();
 
-            var result = GetEmployeesFromResearchAndDevelopment(context);
+            var result = GetEmployeesInPeriod(context);
 
             Console.WriteLine(result);
         }
@@ -85,6 +89,85 @@ namespace SoftUni
             foreach (var employee in employees)
             {
                 sb.AppendLine($"{employee.Name} from {employee.Department} - ${employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 06
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            Employee employeeNakov = context
+                .Employees
+                .First(e => e.LastName == "Nakov");
+
+            employeeNakov.Address = address;
+
+            context.SaveChanges();
+
+            var employeesAddress = context
+                .Employees
+                .OrderByDescending(e => e.AddressId)
+                .Select(e => new
+                {
+                    AddtessText = e.Address.AddressText
+                })
+                .Take(10)
+                .ToList();
+
+            foreach (var employee in employeesAddress)
+            {
+                sb.AppendLine(employee.AddtessText);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .Where(e => e.EmployeesProjects
+                    .Any(p => p.Project.StartDate.Year >= 2001 && p.Project.StartDate.Year <= 2003))
+
+                .Select(e => new
+                {
+                    Name = string.Join(" ", e.FirstName, e.LastName),
+                    Manager = string.Join(" ", e.Manager.FirstName, e.Manager.LastName),
+                    Projects = e.EmployeesProjects
+                        .Select(p => p.Project)
+                        .ToList()
+                })
+                .Take(10)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.Name} - Manager: {employee.Manager}");
+
+                foreach (var project in employee.Projects)
+                {
+                    var startDate = project.StartDate
+                        .ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture);
+
+                    var endDate = project.EndDate == null ?
+                        "not finished" :
+                        project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture);
+                    
+                    sb.AppendLine($"--{project.Name} - {startDate} - {endDate}");
+                    //sb.AppendLine(
+                    //  $"--{project.Name} - {project.StartDate:MM/dd/yyyy} {project.StartDate:h:mm:ss tt} - {(project.EndDate == null ? "not finished" : $"{project.EndDate:MM/dd/yyyy} {project.EndDate:h:mm:ss tt}")}");
+                }
             }
 
             return sb.ToString().TrimEnd();

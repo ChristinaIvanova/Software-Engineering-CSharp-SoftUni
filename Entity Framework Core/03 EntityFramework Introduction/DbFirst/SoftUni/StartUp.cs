fa13af5b46f0250c4d45516f1c0682fe07190afb
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-03 16:52:27 +0200

    Add solution

--- Entity Framework Core/03 EntityFramework Introduction/DbFirst/SoftUni/StartUp.cs
+++ Entity Framework Core/03 EntityFramework Introduction/DbFirst/SoftUni/StartUp.cs
@@ -11,11 +11,12 @@ namespace SoftUni
         {
             var context = new SoftUniContext();
 
-            var result = GetEmployeesWithSalaryOver50000(context);
+            var result = GetEmployeesFromResearchAndDevelopment(context);
 
             Console.WriteLine(result);
         }
 
+        //Problem 03
         public static string GetEmployeesFullInformation(SoftUniContext context)
         {
             var sb = new StringBuilder();
@@ -39,9 +40,10 @@ namespace SoftUni
             return sb.ToString().TrimEnd();
         }
 
+        //Problem 04
         public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
         {
-            var sb=new StringBuilder();
+            var sb = new StringBuilder();
 
             var employees = context
                 .Employees
@@ -57,6 +59,32 @@ namespace SoftUni
             foreach (var employee in employees)
             {
                 sb.AppendLine($"{employee.Name} - {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 05
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new
+                {
+                    Name = string.Join(" ", e.FirstName, e.LastName),
+                    Department = e.Department.Name,
+                    Salary = e.Salary
+                })
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.Name} from {employee.Department} - ${employee.Salary:f2}");
             }
 
             return sb.ToString().TrimEnd();

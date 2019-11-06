2a1841e78afabde5935d60c8eec7df83ed061f12
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-07 00:35:04 +0200

    Add solutions

---
+++ Entity Framework Core/04 Code-First/Code-First/P01_HospitalDatabase/Data/DataSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data
+{
+    public static class DataSettings
+    {
+        public const string DefaultConnection =
+            @"Server=localhost\SQLEXPRESS08;Database=Hospital;Integrated Security = True;";
+    }
+}
---
+++ Entity Framework Core/04 Code-First/Code-First/P01_HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext()
+        {
+        }
+
+        public HospitalContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<Visitation> Visitations { get; set; }
+
+        public DbSet<Medicament> Medicaments { get; set; }
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+
+        public DbSet<PatientMedicament> PatientMedicaments { get; set; }
+
+        public  DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DataSettings.DefaultConnection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>()
+                .HasMany(p => p.Visitations)
+                .WithOne(v => v.Patient)
+                .HasForeignKey(v => v.PatientId);
+
+            modelBuilder.Entity<Patient>()
+                .HasMany(p => p.Diagnoses)
+                .WithOne(d => d.Patient)
+                .HasForeignKey(p => p.PatientId);
+
+            modelBuilder.Entity<PatientMedicament>()
+                .HasKey(pm => new { pm.PatientId, pm.MedicamentId });
+
+            modelBuilder.Entity<PatientMedicament>()
+                .HasOne(pm => pm.Patient)
+                .WithMany(p => p.Prescriptions)
+                .HasForeignKey(pm => pm.PatientId);
+
+            modelBuilder.Entity<PatientMedicament>()
+                .HasOne(pm => pm.Medicament)
+                .WithMany(m => m.Prescriptions)
+                .HasForeignKey(pm => pm.MedicamentId);
+
+            modelBuilder.Entity<Doctor>()
+                .HasMany(d => d.Visitations)
+                .WithOne(v => v.Doctor);
+
+            modelBuilder.Entity<Visitation>()
+                .HasOne(v => v.Doctor)
+                .WithMany(d => d.Visitations)
+                .HasForeignKey(v => v.DoctorId);
+        }
+    }
+}

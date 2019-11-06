d47d6507ee31e5a679a3274f5f84e03766db9ed9
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-07 00:34:30 +0200

    Add solution

---
+++ Entity Framework Core/04 Code-First/Code-First/P01_HospitalDatabase/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data;
+
+namespace P01_HospitalDatabase
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new HospitalContext())
+            {
+                db.Database.Migrate();
+            }
+        }
+    }
+}

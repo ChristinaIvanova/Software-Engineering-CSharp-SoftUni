cbe3293a3455053972fd4d4312e54ce9ad861f7c
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-07 11:13:58 +0200

    Add models

---
+++ Entity Framework Core/04 Code-First/Code-First/P03_SalesDatabase/Data/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data
+{
+    public static class Configuration
+    {
+        public const string DefaultConnection =
+            @"Server=localhost\SQLEXPRESS08;Database=SalesDb;Integrated Security = True;";
+    }
+}
---
+++ Entity Framework Core/04 Code-First/Code-First/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+        }
+
+        public SalesContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.DefaultConnection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Product)
+                .WithMany(p => p.Sales)
+                .HasForeignKey(s => s.ProductId);
+
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Customer)
+                .WithMany(c => c.Sales)
+                .HasForeignKey(s => s.CustomerId);
+
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Store)
+                .WithMany(st => st.Sales)
+                .HasForeignKey(s => s.StoreId);
+
+            //modelBuilder.Entity<Sale>()
+            //    .Property(s => s.Date)
+            //    .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}

1fd8d4fcfb253fe1734b3c28aa5594807a2dd027
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-07 11:14:23 +0200

    Add models

---
+++ Entity Framework Core/04 Code-First/Code-First/P03_SalesDatabase/Data/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Quantity { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [MaxLength(250)]
+        [DefaultValue("No description")]
+        public string Description { get; set; }
+
+        public ICollection<Sale> Sales { get; set; } = new HashSet<Sale>();
+    }
+}
---
+++ Entity Framework Core/04 Code-First/Code-First/P03_SalesDatabase/Data/Models/Sale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlClient;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Query.Expressions;
+using Microsoft.SqlServer.Server;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Sale
+    {
+        [Key]
+        public int SaleId { get; set; }
+        
+        public DateTime Date { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        [Required]
+        public int StoreId { get; set; }
+
+        public Store Store { get; set; }
+    }
+}

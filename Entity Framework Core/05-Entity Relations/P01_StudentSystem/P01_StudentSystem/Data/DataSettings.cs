b1b41ca9cbfa24e0be964e6c1cb667287faab291
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-11 00:04:18 +0200

    Add solution

---
+++ Entity Framework Core/05-Entity Relations/P01_StudentSystem/P01_StudentSystem/Data/DataSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    public static class DataSettings
+    {
+        public static string DefaultConnection =
+            @"Server=localhost\SQLEXPRESS08;DataBase=StudentSystem;Integrated Security=True;";
+    }
+}

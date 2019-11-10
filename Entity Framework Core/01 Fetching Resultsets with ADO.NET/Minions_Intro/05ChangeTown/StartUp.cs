f92b2b185ebc88ab7c7fd0c503e3aee974f8c982
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-10 23:58:15 +0200

    Add solutions

---
+++ Entity Framework Core/01 Fetching Resultsets with ADO.NET/Minions_Intro/05ChangeTown/StartUp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace _05ChangeTown
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string countryName = Console.ReadLine();
+
+            string connectionString = @"Server=localhost\SQLEXPRESS08;" +
+                                      "Database=MinionsDB;" +
+                                      "Integrated Security=true";
+
+            SqlConnection dbCon = new SqlConnection(connectionString);
+            dbCon.Open();
+
+            using (dbCon)
+            {
+                int? countryId = (int?) new SqlCommand($"SELECT Id FROM Countries WHERE Name = '{countryName}'", dbCon)
+                    .ExecuteScalar();
+
+                if (countryId == null)
+                {
+                    Console.WriteLine("No town names were affected.");
+                }
+                else
+                {
+                    SqlDataReader reader = new SqlCommand($"SELECT * FROM Towns WHERE CountryCode = {countryId}", dbCon)
+                        .ExecuteReader();
+
+                    var townNamesAffected = new List<String>();
+
+                    using (reader)
+                    {
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("No town names were affected.");
+                            reader.Close();
+                            dbCon.Close();
+                            return;
+                        }
+
+                        while (reader.Read())
+                        {
+
+                            string townName = (string) reader["Name"];
+
+                            townNamesAffected.Add(townName.ToUpper());
+                        }
+                    }
+
+                    var queryText = @"UPDATE Towns
+                                        SET Name = UPPER(Name)
+                                      WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+
+                    var updateCmnd = new SqlCommand(queryText, dbCon);
+                    updateCmnd.Parameters.AddWithValue("@countryName", countryName);
+                    updateCmnd.ExecuteNonQuery();
+
+                    Console.WriteLine($"{townNamesAffected.Count} town names were affected.");
+                    Console.WriteLine($"[{String.Join(", ", townNamesAffected)}]");
+                }
+            }
+        }
+    }
+}
---
+++ Entity Framework Core/01 Fetching Resultsets with ADO.NET/Minions_Intro/06Remove/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _06Remove
+{
+    class StartUp
+    {
+        private static string connectionString = @"Server=localhost\SQLEXPRESS08;" +
+                                                 "Database=MinionsDB;" +
+                                                 "Integrated Security=true";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            var villainId = int.Parse(Console.ReadLine());
+
+            connection.Open();
+
+            using (connection)
+            {
+                var command = new SqlCommand("SELECT Id FROM Villains WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", villainId);
+
+                int? result = (int?) command.ExecuteScalar();
+
+                if (result == null)
+                {
+                    Console.WriteLine("No such villain was found.");
+                    connection.Close();
+                    return;
+                }
+
+                command = new SqlCommand("SELECT COUNT(*) FROM MinionsVillains WHERE VillainId = @Id", connection);
+                command.Parameters.AddWithValue("@Id", villainId);
+                var minionsCount = (int) command.ExecuteScalar();
+
+                command = new SqlCommand("DELETE FROM MinionsVillains WHERE VillainId = @Id", connection);
+                command.Parameters.AddWithValue("@Id", villainId);
+                command.ExecuteNonQuery();
+
+                command = new SqlCommand("SELECT Name FROM Villains WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", villainId);
+                var villainName = (string) command.ExecuteScalar();
+
+                command = new SqlCommand("DELETE FROM Villains WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", villainId);
+                command.ExecuteNonQuery();
+
+                Console.WriteLine($"{villainName} was deleted.");
+                Console.WriteLine($"{minionsCount} minions were released.");
+            }
+        }
+    }
+}

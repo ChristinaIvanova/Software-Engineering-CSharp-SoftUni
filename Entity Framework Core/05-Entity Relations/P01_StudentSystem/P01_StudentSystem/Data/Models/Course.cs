3412774dfb28e318a958a75ab81d762f14db61d3
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-11 00:05:43 +0200

    Add models

---
+++ Entity Framework Core/05-Entity Relations/P01_StudentSystem/P01_StudentSystem/Data/Models/Course.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")]
+        public string Description { get; set; }
+
+        [Column(TypeName = "date")]
+        
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public ICollection<StudentCourse> StudentsEnrolled { get; set; } = new HashSet<StudentCourse>();
+
+        public ICollection<Homework> HomeworkSubmissions { get; set; } = new HashSet<Homework>();
+
+        public ICollection<Resource> Resources { get; set; } = new HashSet<Resource>();
+    }
+}
---
+++ Entity Framework Core/05-Entity Relations/P01_StudentSystem/P01_StudentSystem/Data/Models/Homework.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+   public class Homework
+    {
+        [Key]
+        public int HomeworkId { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(max)")]
+        public string Content { get; set; }
+
+        public ContentType ContentType { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime SubmissionTime { get; set; }
+
+        public int StudentId { get; set; }
+
+        public Student Student { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}

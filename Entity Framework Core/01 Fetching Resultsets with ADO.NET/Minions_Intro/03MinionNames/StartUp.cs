b82c72cee644478a89201572f9c36890aaec5cdd
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-10 23:57:22 +0200

    Add solutions

---
+++ Entity Framework Core/01 Fetching Resultsets with ADO.NET/Minions_Intro/03MinionNames/StartUp.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _03MinionNames
+{
+    class StartUp
+    {
+        private static string connectionString = @"Server=localhost\SQLEXPRESS08;" +
+                                                 "Database=MinionsDB;" +
+                                                 "Integrated Security=true";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Villain ID: ");
+            var villainId = int.Parse(Console.ReadLine());
+
+            using (connection)
+            {
+                connection.Open();
+
+                var queryText = @"SELECT v.Name AS VillainName,
+                                         m.Name AS MinionName,
+                                         m.Age AS MinionAge
+                                  FROM Villains AS v
+                                       LEFT JOIN MinionsVillains AS vm ON vm.VillainId = v.Id
+                                       LEFT JOIN Minions AS m ON m.Id = vm.MinionId
+                                  WHERE v.Id = @Id;";
+
+                var cmnd = new SqlCommand(queryText, connection);
+
+                using (cmnd)
+                {
+                    cmnd.Parameters.AddWithValue("@Id", villainId);
+
+                    var reader = cmnd.ExecuteReader();
+
+                    using (reader)
+                    {
+                        ProcessSelection(reader, villainId);
+                    }
+                }
+            }
+
+        }
+
+        private static void ProcessSelection(SqlDataReader reader, int villainId)
+        {
+            if (reader.HasRows)
+            {
+                reader.Read();
+                Console.WriteLine($"Villain: {reader["VillainName"]}");
+
+                if (reader.IsDBNull(1))
+                {
+                    Console.WriteLine("(no minions)");
+                }
+                else
+                {
+                    int minionNumber = 1;
+                    while (true)
+                    {
+                        Console.WriteLine($"{minionNumber++}. {reader["MinionName"]} {reader["MinionAge"]}");
+
+                        if (!reader.Read())
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No villain with ID {villainId} exists in the database.");
+            }
+        }
+    }
+}
---
+++ Entity Framework Core/01 Fetching Resultsets with ADO.NET/Minions_Intro/04AddMinion/StartUp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _04AddMinion
+{
+    class StartUp
+    {
+        private static string connectionString = @"Server=localhost\SQLEXPRESS08;" +
+                                                 "Database=MinionsDB;" +
+                                                 "Integrated Security=true";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine().Split();
+            var minionName = tokens[1];
+            var minionAge = int.Parse(tokens[2]);
+            var minionTown = tokens[3];
+
+            tokens = Console.ReadLine().Split();
+            var villainName = tokens[1];
+
+            connection.Open();
+
+            using (connection)
+            {
+                var command = new SqlCommand($"SELECT COUNT(*) FROM Towns WHERE Name = '{minionTown}'", connection);
+
+                if ((int)command.ExecuteScalar() == 0)
+                {
+                    command = new SqlCommand($"INSERT INTO Towns(Name) VALUES ('{minionTown}')", connection);
+                    command.ExecuteNonQuery();
+                    Console.WriteLine($"Town {minionTown} was added to the database.");
+                }
+
+                command = new SqlCommand($"SELECT COUNT(*) FROM Villains WHERE Name = '{villainName}'", connection);
+
+                if ((int)command.ExecuteScalar() == 0)
+                {
+                    command = new SqlCommand($"INSERT INTO Villains (Name, EvilnessFactorId) VALUES ('{villainName}', 4)", connection);
+                    command.ExecuteNonQuery();
+                    Console.WriteLine($"Villain {villainName} was added to the database.");
+                }
+
+                command = new SqlCommand($"SELECT Id FROM Towns WHERE Name = '{minionTown}'", connection);
+                var townId = (int)command.ExecuteScalar();
+
+                command = new SqlCommand($"INSERT INTO Minions(Name, Age, TownId) VALUES ('{minionName}', {minionAge}, {townId})", connection);
+                command.ExecuteNonQuery();
+
+                var villainId = (int)new SqlCommand($"SELECT Id FROM Villains WHERE Name = '{villainName}'", connection).ExecuteScalar();
+                var minionId = (int)new SqlCommand($"SELECT Id FROM Minions WHERE Name = '{minionName}'", connection).ExecuteScalar();
+
+                command = new SqlCommand($"INSERT INTO MinionsVillains VALUES ({minionId}, {villainId})", connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine($"Successfully added {minionName} to be minion of {villainName}.");
+            }
+        }
+    }
+}

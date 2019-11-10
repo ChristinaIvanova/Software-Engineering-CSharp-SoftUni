f3624576efc6d8494bc0efbb2df66f632625675b
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-11 00:10:18 +0200

    Add solution

---
+++ Entity Framework Core/05-Entity Relations/P03_FootballBetting/P03_FootballBetting/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_FootballBetting.Data;
+using System;
+
+namespace P03_FootballBetting
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var db=new FootballBettingContext())
+            {
+                db.Database.Migrate();
+            }
+        }
+    }
+}

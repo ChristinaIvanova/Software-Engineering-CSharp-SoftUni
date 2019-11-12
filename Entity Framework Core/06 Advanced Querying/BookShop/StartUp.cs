9f6c8e86f0b8722f57fd32b5d502f75b7ae1d26f
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-12 23:13:14 +0200

    Add method

--- Entity Framework Core/06 Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework Core/06 Advanced Querying/BookShop/StartUp.cs
@@ -18,7 +18,7 @@ namespace BookShop
 
                 var input = Console.ReadLine();
 
-                var result = GetBooksByCategory(db, input);
+                var result = GetBooksByAuthor(db, input);
                 Console.WriteLine(result);
             }
         }
@@ -102,6 +102,76 @@ namespace BookShop
                     .Any(bc => categories.Contains(bc.Category.Name.ToLower())))
                 .Select(b => b.Title)
                 .OrderBy(b => b)
+                .ToList();
+
+            sb.Append(string.Join(Environment.NewLine, books));
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var sb = new StringBuilder();
+
+            var formattedDate = DateTime.ParseExact(date, "dd-MM-yyyy", null);
+
+            var books = context.Books
+                .Where(b => b.ReleaseDate < formattedDate)
+                .OrderByDescending(b => b.ReleaseDate)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.EditionType,
+                    b.Price
+                })
+                .ToList();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} - {book.EditionType} - ${book.Price:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+            var sb = new StringBuilder();
+
+            var authorsList = context.Authors
+                .Where(a => a.FirstName.EndsWith(input))
+                .Select(a => $"{a.FirstName} {a.LastName}")
+                .OrderBy(a => a)
+                .ToList();
+
+            sb.Append(string.Join(Environment.NewLine, authorsList));
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            var sb = new StringBuilder();
+
+            var books = context.Books
+                .Where(b => b.Title.ToLower().Contains(input.ToLower()))
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList();
+
+            sb.Append(string.Join(Environment.NewLine, books));
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetBooksByAuthor(BookShopContext context, string input)
+        {
+            var sb = new StringBuilder();
+
+            var books = context.Books
+                .Where(b => b.Author.LastName.ToLower().StartsWith(input.ToLower()))
+                .OrderBy(b => b.BookId)
+                .Select(b => $"{b.Title} ({b.Author.FirstName} {b.Author.LastName})")
                 .ToList();
 
             sb.Append(string.Join(Environment.NewLine, books));

b6ecf569270c833f10b4c66f49d49c9ca4b7f0d6
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-17 00:49:44 +0200

    Add ViewModels

---
+++ Entity Framework Core/07 Auto Mapping Objects/FastFood.Web/ViewModels/Employees/RegisterEmployeeInputModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Web.ViewModels.Employees
+{
+    public class RegisterEmployeeInputModel
+    {
+        [Required]
+        [MinLength(3), MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(16, 80)]
+        public int Age { get; set; }
+
+        public int PositionId { get; set; }
+
+        public string PositionName { get; set; }
+
+        [Required]
+        [MinLength(3), MaxLength(30)]
+        public string Address { get; set; }
+    }
+}
---
+++ Entity Framework Core/07 Auto Mapping Objects/FastFood.Web/ViewModels/Orders/CreateOrderInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Web.ViewModels.Orders
+{
+    public class CreateOrderInputModel
+    {
+        [Required]
+        [MinLength(3), MaxLength(30)]
+        public string Customer { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public int Quantity { get; set; }
+
+        public string OrderType { get; set; }
+    }
+}

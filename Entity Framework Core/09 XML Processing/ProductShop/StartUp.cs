090ffa9e5d9e44d8c570fac8876abf59333d10c1
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-23 23:39:50 +0200

    Add ExportProductDto

--- Entity Framework Core/09 XML Processing/ProductShop/StartUp.cs
+++ Entity Framework Core/09 XML Processing/ProductShop/StartUp.cs
@@ -1,10 +1,14 @@
 ﻿using AutoMapper;
 using ProductShop.Data;
+using ProductShop.Dtos.Export;
 using ProductShop.Dtos.Import;
 using ProductShop.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace ProductShop
@@ -20,9 +24,9 @@ namespace ProductShop
                 //db.Database.EnsureDeleted();
                 //db.Database.EnsureCreated();
 
-                var inputXml = File.ReadAllText("./../../../Datasets/categories-products.xml");
+                //var inputXml = File.ReadAllText("./../../../Datasets/categories-products.xml");
 
-                var result = ImportCategoryProducts(db, inputXml);
+                var result = GetProductsInRange(db);
                 Console.WriteLine(result);
 
             }
@@ -76,7 +80,7 @@ namespace ProductShop
             var xmlSerializer = new XmlSerializer(typeof(List<ImportCategoryDto>),
                 new XmlRootAttribute("Categories"));
 
-            var categoriesDto = (List<ImportCategoryDto>)xmlSerializer.Deserialize(new StringReader(inputXml));
+            var categoriesDto = (ImportCategoryDto[])xmlSerializer.Deserialize(new StringReader(inputXml));
 
             var categories = new List<Category>();
 
@@ -98,18 +102,18 @@ namespace ProductShop
             var xmlSerializer = new XmlSerializer(typeof(List<ImportCategoryProductDto>),
                 new XmlRootAttribute("CategoryProducts"));
 
-            var categoryProductsDto = (List<ImportCategoryProductDto>)xmlSerializer.Deserialize(new StringReader(inputXml));
+            var categoryProductsDto = ((List<ImportCategoryProductDto>)xmlSerializer.Deserialize(new StringReader(inputXml)));
 
             var categoryProducts = new List<CategoryProduct>();
 
-            foreach (var dto in categoryProductsDto)
+            foreach (var cpDto in categoryProductsDto)
             {
-                var existingCategory = context.Categories.Find(dto.CategoryId);
-                var existingProduct = context.Products.Find(dto.ProductId);
+                var existingCategory = context.Categories.Find(cpDto.CategoryId);
+                var existingProduct = context.Products.Find(cpDto.ProductId);
 
                 if (existingCategory != null && existingProduct != null)
                 {
-                    var categoryProduct = Mapper.Map<CategoryProduct>(dto);
+                    var categoryProduct = Mapper.Map<CategoryProduct>(cpDto);
                     categoryProducts.Add(categoryProduct);
                 }
             }
@@ -118,6 +122,32 @@ namespace ProductShop
             var count = context.SaveChanges();
 
             return $"Successfully imported {count}";
+        }
+
+        //Problem 05
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .OrderBy(p => p.Price)
+                .Select(p => new ExportProductDto
+                {
+                    Name = p.Name,
+                    Price = p.Price,
+                    Buyer = $"{p.Buyer.FirstName} {p.Buyer.LastName}"
+                })
+                .Take(10)
+                .ToArray();
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportProductDto[]),
+                new XmlRootAttribute("Products"));
+
+            var sb = new StringBuilder();
+
+            var namespaces = new XmlSerializerNamespaces(new [] { XmlQualifiedName.Empty });
+            xmlSerializer.Serialize(new StringWriter(sb), products, namespaces);
+
+            return sb.ToString().TrimEnd();
         }
     }
 }

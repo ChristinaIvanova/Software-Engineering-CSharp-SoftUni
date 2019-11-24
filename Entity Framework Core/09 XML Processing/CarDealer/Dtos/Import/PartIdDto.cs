7cdcf313cc7b7830de3c1a8e92f86d16841be372
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-25 00:11:30 +0200

    Add ImportDtos

---
+++ Entity Framework Core/09 XML Processing/CarDealer/Dtos/Import/PartIdDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.Dtos.Import
+{
+    [XmlRoot("parts")]
+    public class PartIdDto
+    {
+        [XmlAttribute("id")]
+        public int PartId { get; set; }
+    }
+}
---
+++ Entity Framework Core/09 XML Processing/CarDealer/Dtos/Import/PartsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.Dtos.Import
+{
+    [XmlType("parts")]
+    public class PartsDto
+    {
+        [XmlElement("partId")]
+        public PartIdDto[] PartsIdDto { get; set; }
+    }
+}

384698d51922a96296dd32873ab55965437f9f5c
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-27 23:06:50 +0200

    Add TemplatePattern

---
+++ Entity Framework Core/10 Design Patterns/Design Patterns/TempaltePattern/Bread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TempaltePattern
+{
+    public abstract class Bread
+    {
+        public abstract void MixIngredients();
+
+        public virtual void Slice()
+        {
+            Console.WriteLine($"Slicing the {GetType().Name} bread!");
+        }
+
+        public virtual void Bake()
+        {
+            Console.WriteLine($"Baking the Sourdough Bread. (20 minutes)");
+        }
+
+        public void Make()
+        {
+            this.MixIngredients();
+            this.Bake();
+            this.Slice();
+        }
+    }
+}
---
+++ Entity Framework Core/10 Design Patterns/Design Patterns/TempaltePattern/Sourdough.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TempaltePattern
+{
+    public class Sourdough : Bread
+    {
+        private const string bakingTime = "20 minutes";
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for Sourdough Bread.");
+        }
+    }
+}

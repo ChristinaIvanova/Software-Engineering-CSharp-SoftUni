3c9eb48a2c9fcdf568b288bbd62caa79c95123a1
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-27 23:06:06 +0200

    Add PrototypePattern

---
+++ Entity Framework Core/10 Design Patterns/Design Patterns/PrototypePattern/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var sandwichMenu = new SandwichMenu
+            {
+                ["BLT"] = new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato"),
+                ["PB&J"] = new Sandwich("White", "", "", "PeanutButter, Jelly"),
+                ["Turkey"] = new Sandwich("Rye", "Turkey", "Swiss", "Lettuce, Tomato, Onion")
+            };
+
+
+            var sandwich1 = sandwichMenu["Turkey"].Clone() as Sandwich;
+            var sandwich2 = sandwichMenu["BLT"].Clone() as Sandwich;
+            var sandwich3 = sandwichMenu["PB&J"].Clone() as Sandwich;
+        }
+    }
+}

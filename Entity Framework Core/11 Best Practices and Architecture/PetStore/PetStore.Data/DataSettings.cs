b1b2718d546da136a64b949632e8131f5c342aa9
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-29 01:24:16 +0200

    Add PetStoreDbContext

---
+++ Entity Framework Core/11 Best Practices and Architecture/PetStore/PetStore.Data/DataSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStore.Data
+{
+  public  static class DataSettings
+  {
+      public const string Connection =
+          @"Server=localhost\SQLEXPRESS08;Database=PetStore;Integrated Security=True;";
+  }
+}

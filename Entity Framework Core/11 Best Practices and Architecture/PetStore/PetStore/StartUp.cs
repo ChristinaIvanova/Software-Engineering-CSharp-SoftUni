18edf0362ec10b01a10380550668faaf517b686c
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-29 02:15:59 +0200

    Add StartUp

---
+++ Entity Framework Core/11 Best Practices and Architecture/PetStore/PetStore/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PetStore.Data;
+using PetStore.Services.Implementations;
+
+namespace PetStore
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var data = new PetStoreDbContext();
+
+            var brandService=new BrandService()
+        }
+    }
+}

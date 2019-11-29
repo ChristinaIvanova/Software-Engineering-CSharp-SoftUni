89f6e0bf391b602f9e663c9dc73fb9db09968740
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-29 02:18:09 +0200

    Add App

---
+++ Entity Framework Core/02 ORM Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+
+namespace MiniORM.App
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=localhost\SQLEXPRESS08;" +
+                                   "Database=MiniORM;" +
+                                   "Integrated Security=true";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            //context.Employees.Add(new Employee
+            //{
+            //    FirstName = "Gosho",
+            //    LastName ="Inserted",
+            //    DepartmentId =context.Departments.First().Id,
+            //    IsEmployed = true,
+            //});
+
+            //var employee = context.Employees.Last();
+            //employee.FirstName = "Modified";
+
+            context.Employees.Clear();
+
+            context.SaveChanges();
+        }
+    }
+}

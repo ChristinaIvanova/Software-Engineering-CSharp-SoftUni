5c345988d2ddb6ecfbb4a739fad1c5560f3095c1
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-29 02:14:21 +0200

    Add services

---
+++ Entity Framework Core/11 Best Practices and Architecture/PetStore/PetStore.Services/IBrandService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PetStore.Services.Models;
+using PetStore.Services.Models.Brand;
+
+namespace PetStore.Services
+{
+    public interface IBrandService
+    {
+        int Create(string name);
+
+        IEnumerable<BrandServiceListingModel> SearchByName(string name);
+    }
+}

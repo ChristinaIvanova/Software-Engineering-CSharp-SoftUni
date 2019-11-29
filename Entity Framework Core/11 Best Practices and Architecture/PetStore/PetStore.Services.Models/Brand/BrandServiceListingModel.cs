014e34dd2eb78610b46f09340bdd8440ecbd6843
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2019-11-29 02:15:22 +0200

    Add Brand/BrandServiceListingModel.cs

---
+++ Entity Framework Core/11 Best Practices and Architecture/PetStore/PetStore.Services.Models/Brand/BrandServiceListingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PetStore.Services.Models.Brand
+{
+    public class BrandServiceListingModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}

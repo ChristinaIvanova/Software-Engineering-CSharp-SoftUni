4d0bf68c526b8dbf28f065cb8525e4c6fa2895ff
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2020-01-06 10:21:07 +0200

    Add Brand/BrandWithToysServiceModel.cs

---
+++ Entity Framework Core/11 Best Practices and Architecture/PetStore/PetStore.Services.Models/Brand/BrandWithToysServiceModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PetStore.Services.Models.Toy;
+
+namespace PetStore.Services.Models.Brand
+{
+    public class BrandWithToysServiceModel
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<ToyListing> Toys { get; set; }
+
+    }
+}

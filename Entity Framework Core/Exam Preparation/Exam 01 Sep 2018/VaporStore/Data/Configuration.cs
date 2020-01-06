5c64c099be46d9e6a0825c3462a3b601a00d08be
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2020-01-06 10:28:47 +0200

    Add DataDb

---
+++ Entity Framework Core/Exam Preparation/Exam 01 Sep 2018/VaporStore/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace VaporStore.Data
+{
+	public static class Configuration
+	{
+		public static string ConnectionString =
+			@"Server=localhost\SQLEXPRESS08;Database=VaporStore;Integrated Security=True";
+	}
+}

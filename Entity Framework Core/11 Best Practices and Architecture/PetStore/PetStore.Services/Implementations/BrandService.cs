475481e20da163e9d0656c7b2e2384726a424af6
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2020-01-06 10:23:38 +0200

    Add ServicesImplementation

---
+++ Entity Framework Core/11 Best Practices and Architecture/PetStore/PetStore.Services/Implementations/BrandService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PetStore.Data;
+using PetStore.Data.Models;
+using PetStore.Services.Models.Brand;
+using PetStore.Services.Models.Toy;
+
+namespace PetStore.Services.Implementations
+{
+    public class BrandService : IBrandService
+    {
+        private readonly PetStoreDbContext data;
+
+        public BrandService(PetStoreDbContext data)
+        => this.data = new PetStoreDbContext();
+
+        public int Create(string name)
+        {
+            if (name == null)
+            {
+                throw new InvalidOperationException($"Name cannot be null");
+            }
+
+            if (this.data.Brands.Any(br => br.Name == name))
+            {
+                throw new InvalidOperationException($"{name} already exists");
+            }
+
+            if (name.Length > DataValidation.NameMaxLength)
+            {
+                throw new InvalidOperationException($"Name cannot be more than {DataValidation.NameMaxLength} symbols.");
+            }
+
+            var brand = new Brand
+            {
+                Name = name
+            };
+
+            this.data.Brands.Add(brand);
+            this.data.SaveChanges();
+
+            return brand.Id;
+
+        }
+
+        public IEnumerable<BrandServiceListingModel> SearchByName(string name)
+       => this.data
+                .Brands
+                .Where(br => br.Name.ToLower().Contains(name.ToLower()))
+                .Select(br => new BrandServiceListingModel
+                {
+                    Id = br.Id,
+                    Name = br.Name
+                })
+                .ToList();
+
+        public BrandWithToysServiceModel FindByIdWithToys(int id)
+            => this.data
+                .Brands
+                .Where(br => br.Id == id)
+                .Select(br => new BrandWithToysServiceModel
+                {
+                    Name = br.Name,
+                    Toys = br.Toys
+                        .Select(t => new ToyListing
+                        {
+                            Id = t.Id,
+                            Name = t.Name,
+                            Price = t.Price,
+                            TotalOrders = t.Orders.Count
+                        })
+                })
+                .FirstOrDefault();
+    }
+}
---
+++ Entity Framework Core/11 Best Practices and Architecture/PetStore/PetStore.Services/Implementations/FoodService.cs
@@ -0,0 +1,69 @@
+﻿using PetStore.Data;
+using PetStore.Data.Models;
+using PetStore.Services.Models.Food;
+using System;
+
+namespace PetStore.Services.Implementations
+{
+    public class FoodService : IFoodService
+    {
+        private readonly PetStoreDbContext data;
+
+        public FoodService(PetStoreDbContext data)
+            => this.data = new PetStoreDbContext();
+        public void BuyFromDistributor(string name, double weight, decimal price,
+            double profitRate, DateTime expirationDate, int brandId, int categoryId)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be null or white space");
+            }
+
+            if (profitRate < 0 || profitRate > 5)
+            {
+                throw new ArgumentException("Profit must be higher than 0 and lower than 500%");
+            }
+
+            var food = new Food
+            {
+                Name = name,
+                Weight = weight,
+                DistributorPrice = price,
+                Price = price * (decimal)(1 + profitRate),
+                ExpirationDate = expirationDate,
+                BrandId = brandId,
+                CategoryId = categoryId
+            };
+
+            this.data.Foods.Add(food);
+            this.data.SaveChanges();
+        }
+
+        public void BuyFromDistributor(AddingFoodServiceModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.Name))
+            {
+                throw new ArgumentException("Name cannot be null or white space");
+            }
+
+            if (model.ProfitRate < 0 || model.ProfitRate > 5)
+            {
+                throw new ArgumentException("Profit must be higher than 0 and lower than 500%");
+            }
+
+            var food = new Food
+            {
+                Name = model.Name,
+                Weight = model.Weight,
+                DistributorPrice = model.Price,
+                Price = model.Price * (decimal)(1 + model.ProfitRate),
+                ExpirationDate = model.ExpirationDate,
+                BrandId = model.BrandId,
+                CategoryId = model.CategoryId
+            };
+
+            this.data.Foods.Add(food);
+            this.data.SaveChanges();
+        }
+    }
+}

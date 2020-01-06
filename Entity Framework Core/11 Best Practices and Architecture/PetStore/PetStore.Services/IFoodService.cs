ebc25587b374b88aa4aa6645d05accaf4f25f822
ChristinaIvanova <ivanova.kristina.iv@gmail.com>
2020-01-06 10:22:59 +0200

    Add Interface FoodService

---
+++ Entity Framework Core/11 Best Practices and Architecture/PetStore/PetStore.Services/IFoodService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PetStore.Services.Models.Food;
+
+namespace PetStore.Services
+{
+    public interface IFoodService
+    {
+        void BuyFromDistributor(string name, double weight, decimal price,
+            double profitRate, DateTime expirationDate, int brandId, int categoryId);
+
+        void BuyFromDistributor(AddingFoodServiceModel model);
+    }
+}
